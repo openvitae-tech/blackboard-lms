cef3041ae6303a7a4ad2b32d79ebec7bf795df5c
Deepak Kumar <deepakkumarnd@gmail.com>
2024-05-21 20:47:40 +0530

    Add vimeo player to stream video

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
 
+  VIMEO_REGEXP = /\Ahttps:\/\/vimeo\.com\/\d{9}?.*/
+  VIMEO_BASE_URL = "https://player.vimeo.com/video/"
+
   def header_options(opts={})
     default_header_options = {
       title: nil,
@@ -11,5 +14,11 @@ module ApplicationHelper
 
   def active(name, link)
     link == name ? "active" : ""
+  end
+
+  def vimeo_video_url(url)
+    return "" unless url.match VIMEO_REGEXP
+    video_id = URI(url).path.slice(1..9)
+    "#{VIMEO_BASE_URL}#{video_id}"
   end
 end
