4c26aeba260a83d90c95fe4367b1ddd51cad8bf1
Deepak Kumar <deepakkumarnd@gmail.com>
2024-05-31 23:39:52 +0530

    Add google analytics

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,17 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+document.addEventListener("turbo:load", (event) => {
+    const ga_id = document.querySelector("meta[name='google-analytics-id']").content;
+    if (ga_id) {
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', ga_id);
+    }
+});
