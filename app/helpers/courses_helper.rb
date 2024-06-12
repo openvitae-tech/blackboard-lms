e70314b91864bee667dad39477a6efd238d4229b
Deepak Kumar <deepakkumarnd@gmail.com>
2024-06-12 15:16:40 +0530

    Update loading of next and previous modules as per sequence number

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -14,6 +14,11 @@ module CoursesHelper
   def next_lesson_path(course, course_module, current_lesson)
     next_lesson = course_module.next_lesson(current_lesson)
 
+    if next_lesson.blank?
+      course_module = course_module.next_module
+      next_lesson = course_module.first_lesson if course_module.present?
+    end
+
     if next_lesson.present?
       course_module_lesson_path(course, course_module, next_lesson)
     else
@@ -23,6 +28,11 @@ module CoursesHelper
 
   def prev_lesson_path(course, course_module, current_lesson)
     prev_lesson = course_module.prev_lesson(current_lesson)
+
+    if prev_lesson.blank?
+      course_module = course_module.prev_module
+      prev_lesson = course_module.last_lesson if course_module.present?
+    end
 
     if prev_lesson.present?
       course_module_lesson_path(course, course_module, prev_lesson)
--- app/models/course_module.rb
+++ app/models/course_module.rb
@@ -7,6 +7,14 @@ class CourseModule < ApplicationRecord
     lessons.map(&:duration).reduce(&:+)
   end
 
+  def first_lesson
+    lessons.where(seq_no: 1).first if lessons_count > 0
+  end
+
+  def last_lesson
+    lessons.where(seq_no: lessons_count).first if lessons_count > 0
+  end
+
   def next_lesson(current_lesson)
     if current_lesson.seq_no < lessons_count
       lessons.where(seq_no: current_lesson.seq_no + 1).first
@@ -14,8 +22,28 @@ class CourseModule < ApplicationRecord
   end
 
   def prev_lesson(current_lesson)
-    if current_lesson.seq_no > 0
+    if current_lesson.seq_no > 1
       lessons.where(seq_no: current_lesson.seq_no - 1).first
+    end
+  end
+
+  def first_module
+    self.course.course_modules.where(seq_no: 1).first if self.course.course_modules_count > 0
+  end
+
+  def last_module
+    self.course.course_modules.where(seq_no: self.course.course_modules_count).first if self.course.course_modules_count > 0
+  end
+
+  def next_module
+    if self.seq_no < self.course.course_modules_count
+      self.course.course_modules.where(seq_no: self.seq_no + 1).first
+    end
+  end
+
+  def prev_module
+    if self.seq_no > 1
+      self.course.course_modules.where(seq_no: self.seq_no - 1).first
     end
   end
 end
