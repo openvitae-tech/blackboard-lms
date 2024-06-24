123118ce19dfa8daba8d380228a340a6debe653d
Allen Francis <lallenfrancisl@gmail.com>
2024-06-25 04:41:46 +0530

    Implement home page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,21 +1,24 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-    './node_modules/flowbite/**/*.js'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      fontSize: {
+        xxl: ["24px", "32px"],
+      },
       backgroundImage: {
-        'course-thumb': "url('course_thumbnail.jpeg')"
-      }
+        "course-thumb": "url('course_thumbnail.jpeg')",
+      },
     },
     colors: {
       gray: {
@@ -28,17 +31,20 @@ module.exports = {
         600: "#4B5563",
         700: "#374151",
         800: "#1F2937",
-        900: "#111827"
+        900: "#111827",
       },
       bl: { 100: "#3456DA" },
-      notice: { 100: "#C2BF1A"}
-    }
+      notice: { 100: "#C2BF1A" },
+    },
+    dropShadow: {
+      sm: "0px 2px 6px 0px #1018280F",
+    },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    require('flowbite/plugin')
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+    require("flowbite/plugin"),
   ],
-}
+};
