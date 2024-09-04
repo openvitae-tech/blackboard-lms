4857180602678e14f912b7e0b6d2530654cfaa0f
Deepak Kumar <deepakkumarnd@gmail.com>
2024-09-04 10:24:02 +0530

    Add missing schema changes for the otp changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_23_114058) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_03_112603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -205,6 +205,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_23_114058) do
     t.string "phone"
     t.integer "enrollments_count", default: 0
     t.bigint "team_id"
+    t.integer "otp"
+    t.datetime "otp_generated_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["learning_partner_id"], name: "index_users_on_learning_partner_id"
