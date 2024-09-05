5739efa2237f16b8e11d24ba8654ff48c5c47bdd
Deepak Kumar <deepakkumarnd@gmail.com>
2024-09-05 11:21:44 +0530

    Add service class for login with otp

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -7,10 +7,11 @@ class LoginsController < ApplicationController
   def otp
     @mobile_number = params[:mobile_number]
     user_exists = User.exists?(phone: @mobile_number)
+    service = LoginWithOtpService.instance
 
     if user_exists
       user = User.where(phone: @mobile_number).last
-      user.set_otp!
+      service.set_otp!(user)
     else
       redirect_to new_login_path,  notice: "Incorrect mobile number"
     end
@@ -18,11 +19,13 @@ class LoginsController < ApplicationController
   def create
     user_exists = User.exists?(phone: login_params[:mobile_number])
 
+    service = LoginWithOtpService.instance
+
     if user_exists
       user = User.where(phone: login_params[:mobile_number]).last
 
-      if user.otp_generated_at > 5.minute.ago && user.otp.to_s == login_params[:otp]
-        build_user_session!(user)
+      if service.valid_otp?(user, login_params[:otp])
+        build_user_session(user)
         redirect_to after_sign_in_path_for(user)
       else
         redirect_to new_login_path,  notice: "Incorrect otp or otp expired"
@@ -38,7 +41,7 @@ class LoginsController < ApplicationController
     params.require(:login).permit(:mobile_number, :otp)
   end
 
-  def build_user_session!(user)
+  def build_user_session(user)
     sign_in user
   end
 end
---
+++ app/services/login_with_otp_service.rb
@@ -0,0 +1,11 @@
+class LoginWithOtpService
+  include Singleton
+
+  def set_otp!(user)
+    user.set_otp!
+  end
+
+  def valid_otp?(user, otp)
+    user.otp_generated_at > 5.minute.ago && user.otp.to_s == otp
+  end
+end
