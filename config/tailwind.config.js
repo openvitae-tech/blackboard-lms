5eb96ae830f847347e987890d33641bd76ca8b35
sadrisya <sadrisyalohid@gmail.com>
2024-09-10 20:27:40 +0530

    color namings changes and drop shadow added

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,20 +15,20 @@ module.exports = {
       },
     },
     colors: {
-      primary: "#005F6B",
-      primaryLight:"#4C9A9A",
-      primaryLight50:"#EEF7F7",
-      secondary: "#585563",
-      borderColour:"#E0E0E0",
-      borderColourLight:"#E6E6E6",
-      danger:"#FF2D1A",
-      dangerLight:"#FFF1F0",
-      textLight:"#5C5C5C",
-      white: "#FFFFFF",
-      gold:"#FFD700",
-      goldLight:"#FFFBE5",
-      slateGrey:"#708090",
-      slateGrey50:"#F1F2F4",
+      'primary': "#005F6B",
+      "primary-light":"#4C9A9A",
+      "primary-light-50":"#EEF7F7",
+      "secondary": "#585563",
+      "line-colour":"#E0E0E0",
+      "line-colour-light":"#E6E6E6",
+      "danger":"#FF2D1A",
+      "danger-light":"#FFF1F0",
+      "letter-color-light":"#5C5C5C",
+      "white": "#FFFFFF",
+      "gold":"#FFD700",
+      "gold-light":"#FFFBE5",
+      "slate-grey":"#708090",
+      "slate-grey-light":"#F1F2F4",
 
       white1: "#F6FAFF",
       primary0:"#EAF4F7",
@@ -37,7 +37,9 @@ module.exports = {
     dropShadow: {
       sm: "0px 2px 6px 0px #1018280F",
       medium:"0px 0px 4px 0px #33333329",
-
+      high: "0px 2px 12px 0px #33333329",
+      
+     
     },
   },
   plugins: [
