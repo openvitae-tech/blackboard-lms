c08814f22bf5d046066705b45b577e583ee234ca
Deepak Kumar <deepakkumarnd@gmail.com>
2024-09-19 22:34:44 +0530

    add layout changes in admin pages

---
+++ app/services/learning_partner_onboarding_service.rb
@@ -0,0 +1,11 @@
+
+class LearningPartnerOnboardingService
+  include Singleton
+
+  # def create_partner(partner)
+  #   ActiveRecord::Base.transaction do
+  #     partner.save!
+  #     Team.create!(name: "Top level team", banner: partner.banner, learning_partner: partner)
+  #   end
+  # end
+end
