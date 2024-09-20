e8d06a29b6e64362d7ce27c20c15df10f32db324
sadrisya <sadrisyalohid@gmail.com>
2024-09-20 11:06:01 +0530

    naming convetions:used kebabs for naming ids

--- app/javascript/controllers/teams_controller.js
+++ app/javascript/controllers/teams_controller.js
@@ -1,23 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
-document.querySelectorAll('#addUserBtn, #addTeamBtn').forEach(function(button) {
+document.querySelectorAll('#add-user-btn, #add-team-btn').forEach(function(button) {
     button.addEventListener('click', function() {
-        if (button.id === 'addUserBtn') {
-            document.getElementById('inviteUserPopup').classList.remove('hidden');
-        } else if (button.id === 'addTeamBtn') {
-            document.getElementById('addTeamPopup').classList.remove('hidden');
+        if (button.id === 'add-user-btn') {
+            document.getElementById('invite-user-popup').classList.remove('hidden');
+        } else if (button.id === 'add-team-btn') {
+            document.getElementById('add-team-popup').classList.remove('hidden');
         }
     });
 });
 
 
-document.querySelectorAll('#userCancelBtn, #cancelBtnUser, #teamCancelBtn, #cancelBtnTeam')
+document.querySelectorAll('#user-cancel-btn, #cancel-btn-user, #team-cancel-btn, #cancel-btn-team')
     .forEach(function(button) {
         button.addEventListener('click', function() {
-            if (button.id === 'userCancelBtn' || button.id === 'cancelBtnUser') {
-                document.getElementById('inviteUserPopup').classList.add('hidden');
-            } else if (button.id === 'teamCancelBtn' || button.id === 'cancelBtnTeam') {
-                document.getElementById('addTeamPopup').classList.add('hidden');
+            if (button.id === 'user-cancel-btn' || button.id === 'cancel-btn-user') {
+                document.getElementById('invite-user-popup').classList.add('hidden');
+            } else if (button.id === 'team-cancel-btn' || button.id === 'cancel-btn-team') {
+                document.getElementById('add-team-popup').classList.add('hidden');
             }
         });
     });
@@ -29,27 +29,27 @@ const createFileUploadComponent = (containerId, labelText) => {
         <div class="flex flex-col gap-4 items-start justify-center w-full">
             <div class="flex flex-row gap-2 justify-between w-full">
                 <label class="text-sm">${labelText}</label>
-                <label class="text-sm text-[#E0E0E0]" id="${containerId}-fileLabel">No file chosen</label>
+                <label class="text-sm text-[#E0E0E0]" id="${containerId}-file-label">No file chosen</label>
             </div>
-            <div id="${containerId}-chooseFileDiv" class="flex gap-4 justify-center p-4 w-full border border-dashed border-primary rounded cursor-pointer">
+            <div id="${containerId}-choose-file-div" class="flex gap-4 justify-center p-4 w-full border border-dashed border-primary rounded cursor-pointer">
                 <span class="icon icon-upload h-4 w-4"></span>
                 <p class="text-sm text-primary">Choose File</p>
             </div>
-            <input type="file" id="${containerId}-fileInput" class="hidden" />
+            <input type="file" id="${containerId}-file-input" class="hidden" />
         </div>
     `;
 
-    document.getElementById(`${containerId}-chooseFileDiv`).addEventListener('click', () =>
-        document.getElementById(`${containerId}-fileInput`).click()
+    document.getElementById(`${containerId}-choose-file-div`).addEventListener('click', () =>
+        document.getElementById(`${containerId}-file-input`).click()
     );
 
-    document.getElementById(`${containerId}-fileInput`).addEventListener('change', (event) => {
+    document.getElementById(`${containerId}-file-input`).addEventListener('change', (event) => {
         const fileName = event.target.files[0]?.name || 'No file chosen';
-        document.getElementById(`${containerId}-fileLabel`).textContent = fileName;
+        document.getElementById(`${containerId}-file-label`).textContent = fileName;
     });
 };
 
-createFileUploadComponent('uploadCSV', 'Upload CSV');
-createFileUploadComponent('uploadYourLogo', 'Upload Your Logo');
+createFileUploadComponent('upload-csv', 'Upload CSV');
+createFileUploadComponent('upload-your-logo', 'Upload Your Logo');
     
     
