a3bcac3a6df94b899a4584ac56ce0e16e28a96df
sadrisya <sadrisyalohid@gmail.com>
2024-10-08 14:46:57 +0530

    Course description show more and less icon class swith is managed

--- app/javascript/controllers/course_description_card_controller.js
+++ app/javascript/controllers/course_description_card_controller.js
@@ -11,14 +11,21 @@ export default class extends Controller {
         console.log(this.showMoreToggleTarget);
         if (this.courseDescriptionTarget.classList.contains("show-more-desc")) {
             this.showMoreToggleTarget.firstElementChild.innerHTML = "Show more"
+
             this.upArrowTarget.classList.add("hidden")
+            this.upArrowTarget.classList.remove("icon")
             this.downArrowTarget.classList.remove("hidden")
+            this.downArrowTarget.classList.add("icon")
 
             this.courseDescriptionTarget.classList.remove("show-more-desc");
         } else {
             this.showMoreToggleTarget.firstElementChild.innerHTML = "Show less"
+            
             this.upArrowTarget.classList.remove("hidden")
+            this.upArrowTarget.classList.add("icon")
             this.downArrowTarget.classList.add("hidden")
+            this.downArrowTarget.classList.remove("icon")
+
             this.courseDescriptionTarget.classList.add("show-more-desc");
         }
     }
