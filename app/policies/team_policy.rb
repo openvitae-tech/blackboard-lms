5ed85d2669331ba8cc0b9a58088c23eeb707e2a1
Deepak Kumar <deepakkumarnd@gmail.com>
2024-10-18 15:45:50 +0530

    Fix permission for actions in team page, add reusable components for buttons in team page

--- app/policies/team_policy.rb
+++ app/policies/team_policy.rb
@@ -4,28 +4,29 @@ class TeamPolicy < ApplicationPolicy
   attr_reader :user, :record
 
   def initialize(user, record)
+    super
     @user = user
     @record = record
   end
 
   def new?
-    user.is_manager?
+    user.is_manager? || user.is_owner?
   end
 
   def show?
-    user.is_manager?
+    user.is_manager? || user.is_owner?
   end
 
   def create?
-    user.is_manager?
+    user.is_manager? || user.is_owner?
   end
 
   def update?
-    user.is_manager?
+    user.is_manager? || user.is_owner?
   end
 
   def edit?
-    user.is_manager?
+    user.is_manager? || user.is_owner?
   end
 
   def destroy?
