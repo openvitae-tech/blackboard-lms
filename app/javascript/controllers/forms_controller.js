65187ced585062609343b446440c44d5e7b6543b
sadrisya <sadrisyalohid@gmail.com>
2024-10-18 11:54:12 +0530

    chose file component created and used in both partners view and teams view

--- app/javascript/controllers/forms_controller.js
+++ app/javascript/controllers/forms_controller.js
@@ -1,17 +1,62 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["fileInput", "selectedFileName"];
+    static targets = [
+        "fileInput",
+        "selectedFileName",
+        "logoPreview",
+        "logoPreviewContainer",
+        "bannerPreview",
+        "bannerPreviewContainer"
+    ];
 
     connect() {
         this.fileInputTarget.addEventListener("change", (e) => {
-            const fileName = e.target.files[0]?.name || "No file chosen";
-            this.selectedFileNameTarget.innerText = fileName;
+            const file = e.target.files[0];
+            this.updateFileName(file);
+            this.showPreview(file);
         });
     }
 
+    updateFileName(file) {
+        const fileName = file ? file.name : "No file chosen";
+        this.selectedFileNameTarget.innerText = fileName;
+    }
+
+    showPreview(file) {
+        const fileType = this.fileInputTarget.dataset.fileType;
+
+        if (file && file.type.startsWith("image/")) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                if (fileType === "logo") {
+                    this.logoPreviewTarget.src = reader.result;
+                    this.logoPreviewContainerTarget.classList.remove("hidden");
+                } else if (fileType === "banner") {
+                    this.bannerPreviewTarget.src = reader.result;
+                    this.bannerPreviewContainerTarget.classList.remove("hidden");
+                }
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+
+    resetInput(event) {
+        const fileType = event.currentTarget.dataset.fileType;
+
+        if (fileType === "logo") {
+            this.logoPreviewContainerTarget.classList.add("hidden");
+            this.logoPreviewTarget.src = ""; 
+        } else if (fileType === "banner") {
+            this.bannerPreviewContainerTarget.classList.add("hidden");
+            this.bannerPreviewTarget.src = ""; 
+        }
+
+        this.fileInputTarget.value = ""; 
+        this.selectedFileNameTarget.innerText = "No file chosen"; 
+    }
+
     chooseFile(event) {
-        console.log("Clicked this");
         this.fileInputTarget.click();
     }
 }
