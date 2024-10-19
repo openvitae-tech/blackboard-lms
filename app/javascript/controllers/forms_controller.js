f18dd763541873841a9c09741ab18ea77ea2c38d
sadrisya <sadrisyalohid@gmail.com>
2024-10-19 11:41:16 +0530

    chose file component created and used in both partners view and teams… (#106)
    chose file component created and used in both partners view and teams view

--- app/javascript/controllers/forms_controller.js
+++ app/javascript/controllers/forms_controller.js
@@ -1,17 +1,64 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["fileInput", "selectedFileName"];
+  static targets = [
+    "fileInput",
+    "selectedFileName",
+    "preview",
+    "previewContainer",
+  ];
 
-    connect() {
-        this.fileInputTarget.addEventListener("change", (e) => {
-            const fileName = e.target.files[0]?.name || "No file chosen";
-            this.selectedFileNameTarget.innerText = fileName;
-        });
-    }
+  connect() {
+    this.fileInputTarget.addEventListener("change", (e) => {
+      const file = e.target.files[0];
+      this.updateFileName(file);
+      this.showPreview(file);
+    });
+  }
 
-    chooseFile(event) {
-        console.log("Clicked this");
-        this.fileInputTarget.click();
+  updateFileName(file) {
+    this.clearError();
+    const fileName = file ? file.name : "No file chosen";
+    this.selectedFileNameTarget.innerText = fileName;
+  }
+
+  showPreview(file) {
+    const fileType = this.fileInputTarget.dataset.fileType;
+
+    if (file) {
+      if (file.type.startsWith("image/")) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.previewTarget.src = reader.result;
+          this.previewContainerTarget.classList.remove("hidden");
+        };
+        reader.readAsDataURL(file);
+      } else if (file.type.startsWith("video/")) {
+        this.showError("Video files are not allowed.");
+      } else {
+        this.showError("Only image files are allowed.");
+      }
     }
+  }
+
+  showError(message) {
+    this.resetInput();
+    this.selectedFileNameTarget.innerText = message;
+    this.selectedFileNameTarget.style.color = "red";
+  }
+  clearError() {
+    this.selectedFileNameTarget.innerText = "No file chosen";
+    this.selectedFileNameTarget.style.color = "";
+  }
+  resetInput() {
+    this.previewContainerTarget.classList.add("hidden");
+    this.previewTarget.src = "";
+
+    this.fileInputTarget.value = "";
+    this.selectedFileNameTarget.innerText = "No file chosen";
+  }
+
+  chooseFile(event) {
+    this.fileInputTarget.click();
+  }
 }
