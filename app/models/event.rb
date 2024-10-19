ea02349c8aaf5fc513913bfc093cf9e579abfa5a
Deepak Kumar <deepakkumarnd@gmail.com>
2024-10-19 14:24:48 +0530

    Add event logging methods for preset events

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,14 +14,15 @@ class Event < ApplicationRecord
     course_completed
   ].freeze
 
-  OnboardingInitiated = Struct.new(:partner_name, :partner_id, keyword_init: true)
-  OnboardingCompleted = Struct.new(:partner_name, :partner_id, :email, keyword_init: true)
+  OnboardingInitiated = Struct.new(:partner_id, :partner_name,  keyword_init: true)
+  FirstUserJoined = Struct.new(:partner_id, :partner_name, :email, :team_id, :user_id, keyword_init: true)
   UserInvited = Struct.new(:partner_id, :team_id, :user_id, :invite_email, keyword_init: true) # user invited by owner or manager not by admin
   UserJoined = Struct.new(:partner_id, :team_id, :user_id, :invite_email, keyword_init: true) # user joined via an invite
   UserLogin = Struct.new(:partner_id, :team_id, :user_id, keyword_init: true)
   UserLogout = Struct.new(:partner_id, :team_id, :user_id, keyword_init: true)
-  CourseAssigned = Struct.new(:partner_id, :team_id, :assigned_by_user_id, :user_id, :course_id, keyword_init: true)
+  CourseAssigned = Struct.new(:partner_id, :team_id, :user_id, :assigned_to_user, :course_id, keyword_init: true)
   CourseEnrolled = Struct.new(:partner_id, :team_id, :user_id, :course_id, :self_enroll, keyword_init: true)
+  CourseDropped = Struct.new(:partner_id, :team_id, :user_id, :course_id, keyword_init: true)
   CourseStarted = Struct.new(:partner_id, :team_id, :user_id, :course_id, keyword_init: true)
   CourseCompleted = Struct.new(:partner_id, :team_id, :user_id, :course_id, keyword_init: true)
 
