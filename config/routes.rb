9df37990e72fd092a9905e5d852495231cb7b4bc
Deepak Kumar <deepakkumarnd@gmail.com>
2024-10-19 16:47:10 +0530

    Add event for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -73,7 +73,8 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: {
-    confirmations: 'users/confirmations'
+    confirmations: 'users/confirmations',
+    sessions: 'users/sessions'
   }
 
   resources :users
