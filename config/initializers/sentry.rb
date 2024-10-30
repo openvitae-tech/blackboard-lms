1d751aa98aae421c95a7af02c479d52ed5c2aa72
Deepak Kumar <deepakkumarnd@gmail.com>
2024-10-30 15:42:11 +0530

    Enabled few cops for rubocop

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,14 +2,14 @@
 
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for tracing.
   # We recommend adjusting this value in production.
   config.traces_sample_rate = 1.0
   # or
-  config.traces_sampler = lambda do |context|
+  config.traces_sampler = lambda do |_context|
     true
   end
   # Set profiles_sample_rate to profile 100%
