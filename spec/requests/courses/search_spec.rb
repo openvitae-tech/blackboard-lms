983c4faa116a8237a661643ba2ee110083155dd1
Deepak Kumar <deepakkumarnd@gmail.com>
2024-10-31 19:39:01 +0530

    Add requests specs for course search

---
+++ spec/requests/courses/search_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Request spec for GET /courses/search' do
+  describe 'course search by learner, manager or owner' do
+    %i[manager owner].each do |role|
+      before(:each) do
+        @team = create :team
+        @user = create :user, role, team: @team
+        sign_in @user
+        @courses = Array.new(4) { |index| course_with_associations(published: index.even?) }
+      end
+
+      let(:courses) { @courses }
+      let(:user) { @user }
+
+      it 'Returns matching results when searching with a search term' do
+        term = courses[0].title.split[0]
+        get("/courses/search?term=#{term}")
+        expect(response.status).to render_template(:index)
+        expect(assigns[:search_results]).not_to be_empty
+        expect(assigns[:search_results].include?(courses[0])).to be_truthy
+      end
+
+      it 'return no results when there are no match or matching course is unpublished' do
+        term = courses[1].title.split[0]
+        get("/courses/search?term=#{term}")
+        expect(response.status).to render_template(:index)
+        expect(assigns[:search_results].include?(courses[1])).to be_falsey
+      end
+    end
+  end
+
+  describe 'search by admin' do
+    before(:each) do
+      admin = create :user, :admin
+      sign_in admin
+      @courses = Array.new(4) { |index| course_with_associations(published: index.even?) }
+    end
+
+    let(:courses) { @courses }
+
+    it 'Returns matching results when searching with a search term even for unpublished courses' do
+      term = courses[1].title.split[0]
+      get("/courses/search?term=#{term}")
+      expect(response.status).to render_template(:index)
+      expect(assigns[:search_results]).not_to be_empty
+      expect(assigns[:search_results].include?(courses[1])).to be_truthy
+    end
+  end
+end
