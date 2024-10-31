762d8483d206212dcfa691fd306c6bee0eb3f559
Deepak Kumar <deepakkumarnd@gmail.com>
2024-10-31 17:29:51 +0530

    Add request specs for publish and unpublish

--- spec/requests/courses/publish_spec.rb
+++ spec/requests/courses/publish_spec.rb
@@ -31,6 +31,7 @@ RSpec.describe 'Request spec for PUT /courses/:id/publish' do
     it 'Success when admin publishes a course' do
       put("/courses/#{subject.id}/publish")
       expect(response.status).to be(302)
+      expect(flash[:notice]).to eq(I18n.t('course.published'))
       expect(subject.reload.published?).to be_truthy
     end
   end
