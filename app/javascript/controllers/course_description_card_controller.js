4886391e711c3852dea8982cfa9f3ce9161d031f
sadrisya <sadrisyalohid@gmail.com>
2024-11-07 14:41:55 +0530

    Removed an unwanted console.log in course_description_card_controller removed also default formating happened

--- app/javascript/controllers/course_description_card_controller.js
+++ app/javascript/controllers/course_description_card_controller.js
@@ -1,32 +1,35 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = [
+    "showMoreToggle",
+    "upArrow",
+    "downArrow",
+    "courseDescription",
+  ];
+  connect() {
+    console.log("Course card controller active");
+  }
 
-    static targets = ["showMoreToggle","upArrow","downArrow", "courseDescription"];
-    connect() {
-        console.log("Course card controller active")
+  showMoreToggle(event) {
+    if (this.courseDescriptionTarget.classList.contains("show-more-desc")) {
+      this.showMoreToggleTarget.firstElementChild.innerHTML = "Show more";
+
+      this.upArrowTarget.classList.add("hidden");
+      this.upArrowTarget.classList.remove("icon");
+      this.downArrowTarget.classList.remove("hidden");
+      this.downArrowTarget.classList.add("icon");
+
+      this.courseDescriptionTarget.classList.remove("show-more-desc");
+    } else {
+      this.showMoreToggleTarget.firstElementChild.innerHTML = "Show less";
+
+      this.upArrowTarget.classList.remove("hidden");
+      this.upArrowTarget.classList.add("icon");
+      this.downArrowTarget.classList.add("hidden");
+      this.downArrowTarget.classList.remove("icon");
+
+      this.courseDescriptionTarget.classList.add("show-more-desc");
     }
-
-    showMoreToggle(event) {
-        console.log(this.showMoreToggleTarget);
-        if (this.courseDescriptionTarget.classList.contains("show-more-desc")) {
-            this.showMoreToggleTarget.firstElementChild.innerHTML = "Show more"
-
-            this.upArrowTarget.classList.add("hidden")
-            this.upArrowTarget.classList.remove("icon")
-            this.downArrowTarget.classList.remove("hidden")
-            this.downArrowTarget.classList.add("icon")
-
-            this.courseDescriptionTarget.classList.remove("show-more-desc");
-        } else {
-            this.showMoreToggleTarget.firstElementChild.innerHTML = "Show less"
-            
-            this.upArrowTarget.classList.remove("hidden")
-            this.upArrowTarget.classList.add("icon")
-            this.downArrowTarget.classList.add("hidden")
-            this.downArrowTarget.classList.remove("icon")
-
-            this.courseDescriptionTarget.classList.add("show-more-desc");
-        }
-    }
+  }
 }
