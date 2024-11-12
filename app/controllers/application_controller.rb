798355c83b233dfab0ffb626df9361b3d02e2fd7
Paul Sojan <paul.eliassojan@gmail.com>
2024-11-12 11:25:47 +0530

    Moved not found erb to views/pages dir

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def not_found
-    render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+    render template: "pages/not_found", layout: false, status: :not_found
   end
 
   protected
