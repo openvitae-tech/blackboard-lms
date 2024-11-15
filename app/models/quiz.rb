a7446dedd04cba11bcc0e1dae2a68e84cdb8b356
Deepak Kumar <deepakkumarnd@gmail.com>
2024-11-15 16:59:42 +0530

    Fix quiz creation workflow, add validation. Fix form params and form url (#265)

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,4 +3,11 @@
 class Quiz < ApplicationRecord
   belongs_to :course_module, counter_cache: true
   has_many :quiz_answers, dependent: :destroy
+
+  validates :question, presence: true
+  validates :option_a, presence: true
+  validates :option_b, presence: true
+  validates :option_c, presence: true
+  validates :option_d, presence: true
+  validates :answer, presence: true
 end
