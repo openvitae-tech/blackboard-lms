5117b5f533bf9e1b9d1a0e294c8c2aa056b1d113
Deepak Kumar <deepakkumarnd@gmail.com>
2024-11-19 23:06:49 +0530

    add emails while assigning a course to users

--- config/environments/docker.rb
+++ config/environments/docker.rb
@@ -96,4 +96,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.asset_host = Rails.application.credentials.dig(:app, :base_url)
 end
