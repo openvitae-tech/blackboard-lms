185390567db1978be1f8785905496d489da7e739
Deepak Kumar <deepakkumarnd@gmail.com>
2024-11-19 16:46:46 +0530

    set the default format only for not_found action.
    On setting request format for turbo_frame requests fails and models are not working as expected. As a workaroudn the format is set only when the resource is not found.

--- app/controllers/concerns/handle_not_found.rb
+++ app/controllers/concerns/handle_not_found.rb
@@ -4,7 +4,7 @@ module HandleNotFound
   included do
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
-    before_action :set_default_format
+    before_action :set_default_format, only: [:not_found]
 
     def not_found
       render template: "pages/not_found", layout: false, status: :not_found
