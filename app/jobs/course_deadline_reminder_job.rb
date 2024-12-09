c6731f67daf290a5bf9f7c995174b63a377d1175
Deepak Kumar <deepakkumarnd@gmail.com>
2024-12-09 17:05:18 +0530

    Schedule daily cron job to send email alerts for course deadline.

---
+++ app/jobs/course_deadline_reminder_job.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CourseDeadlineReminderJob < BaseJob
+  def perform
+    Rails.logger.info "$"*100
+    enrollments = Enrollment.where(deadline_at: Time.zone.now..3.days.from_now)
+
+    enrollments.each do |enrollment|
+      # send at most one email per day, kept 1 hours as buffer for any retries
+      if enrollment.reminder_send_at.blank? || enrollment.reminder_send_at < 23.hours.ago
+        UserMailer.course_deadline_reminder(enrollment.user, enrollment.course, enrollment.deadline_at).deliver_later
+        enrollment.touch(:reminder_send_at)
+      end
+    end
+  end
+end
