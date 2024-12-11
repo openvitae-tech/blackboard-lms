7628236931d260fbee78c7e59cb60f85292a135e
sadrisya <sadrisyalohid@gmail.com>
2024-12-11 14:00:19 +0530

    more button made as a component

--- app/javascript/controllers/user_status_confirmation_controller.js
+++ app/javascript/controllers/user_status_confirmation_controller.js
@@ -1,31 +1,43 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["inactiveUser", "activeUser", "deactivateMenu","activateConfirmation","deactivateConfirmation"];
+  static targets = [
+    "inactiveUser",
+    "activeUser",
+    "deactivateMenu",
+    "activateConfirmation",
+    "deactivateConfirmation",
+  ];
 
   confirm(event) {
     const buttonDataAction = event.currentTarget.dataset.actionType;
-    if (buttonDataAction === "deactivateYes") {
+    const isActivate = buttonDataAction === "deactivateYes";
+
+    const updateEvent = new CustomEvent("statusChange", {
+      detail: { activate: isActivate },
+      bubbles: true,
+    });
+
+    this.element.dispatchEvent(updateEvent);
+    
+    if (isActivate) {
       this.deactivateConfirmationTarget.classList.add("hidden");
       this.inactiveUserTarget.classList.remove("hidden");
       this.activeUserTarget.classList.add("hidden");
-      this.deactivateMenuTarget.children[0].textContent = "Activate";
+      this.deactivateMenuTarget.textContent = "Activate";
       this.deactivateMenuTarget.setAttribute("data-activate", "true");
-
     } else {
       this.activateConfirmationTarget.classList.add("hidden");
       this.activeUserTarget.classList.remove("hidden");
       this.inactiveUserTarget.classList.add("hidden");
-      this.deactivateMenuTarget.children[0].textContent = "Deactivate";
+      this.deactivateMenuTarget.textContent = "Deactivate";
       this.deactivateMenuTarget.setAttribute("data-activate", "false");
-
     }
   }
-cancel(event) {
-  const buttonDataAction = event.currentTarget.dataset.actionType;
-  buttonDataAction === "deactivateNo"
+  cancel(event) {
+    const buttonDataAction = event.currentTarget.dataset.actionType;
+    buttonDataAction === "deactivateNo"
       ? this.deactivateConfirmationTarget.classList.add("hidden")
       : this.activateConfirmationTarget.classList.add("hidden");
-}
-
+  }
 }
