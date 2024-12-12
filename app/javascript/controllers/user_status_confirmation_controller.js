181c50e317a9e7a5c74e1e15d788652361e1b5ef
sadrisya <sadrisyalohid@gmail.com>
2024-12-12 20:04:11 +0530

    deactivate button misalignment fixed (#414)
    Co-authored-by: Deepak Kumar <deepakkumarnd@gmail.com>

--- app/javascript/controllers/user_status_confirmation_controller.js
+++ app/javascript/controllers/user_status_confirmation_controller.js
@@ -1,31 +1,51 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["inactiveUser", "activeUser", "deactivateMenu","activateConfirmation","deactivateConfirmation"];
+  static targets = [
+    "inactiveUser",
+    "activeUser",
+    "moreMenu",
+    "deactivateMenu",
+    "activateConfirmation",
+    "deactivateConfirmation",
+  ];
+
+  showConfirmation() {
+    console.log(
+      "Updated data-activate:",
+      this.deactivateMenuTarget.getAttribute("data-activate")
+    );
+
+    const isActivate =
+      this.deactivateMenuTarget.getAttribute("data-activate") === "true";
+    isActivate
+      ? this.activateConfirmationTarget.classList.remove("hidden")
+      : this.deactivateConfirmationTarget.classList.remove("hidden");
+    this.moreMenuTarget.classList.add("hidden");
+  }
 
   confirm(event) {
     const buttonDataAction = event.currentTarget.dataset.actionType;
-    if (buttonDataAction === "deactivateYes") {
+    const isActivate = buttonDataAction === "deactivateYes";
+
+    if (isActivate) {
       this.deactivateConfirmationTarget.classList.add("hidden");
       this.inactiveUserTarget.classList.remove("hidden");
       this.activeUserTarget.classList.add("hidden");
-      this.deactivateMenuTarget.children[0].textContent = "Activate";
+      this.deactivateMenuTarget.textContent = "Activate";
       this.deactivateMenuTarget.setAttribute("data-activate", "true");
-
     } else {
       this.activateConfirmationTarget.classList.add("hidden");
       this.activeUserTarget.classList.remove("hidden");
       this.inactiveUserTarget.classList.add("hidden");
-      this.deactivateMenuTarget.children[0].textContent = "Deactivate";
+      this.deactivateMenuTarget.textContent = "Deactivate";
       this.deactivateMenuTarget.setAttribute("data-activate", "false");
-
     }
   }
-cancel(event) {
-  const buttonDataAction = event.currentTarget.dataset.actionType;
-  buttonDataAction === "deactivateNo"
+  cancel(event) {
+    const buttonDataAction = event.currentTarget.dataset.actionType;
+    buttonDataAction === "deactivateNo"
       ? this.deactivateConfirmationTarget.classList.add("hidden")
       : this.activateConfirmationTarget.classList.add("hidden");
-}
-
+  }
 }
