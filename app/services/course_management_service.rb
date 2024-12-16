ace0f1300c9b9b041f7f9106fc25e8bda747bd4f
Deepak Kumar <deepakkumarnd@gmail.com>
2024-12-16 12:01:27 +0530

    Fix tests

--- app/services/course_management_service.rb
+++ app/services/course_management_service.rb
@@ -86,7 +86,7 @@ class CourseManagementService
       next if result != :ok
 
       EVENT_LOGGER.publish_course_assigned(assigned_by, user.id, course.id)
-      Notification.notify(user, format(I18n.t('course.assigned'), course: course.title, name: assigned_by.name))
+      NotificationService.notify(user, format(I18n.t('course.assigned'), course: course.title, name: assigned_by.name))
       UserMailer.course_assignment(user, assigned_by, course).deliver_later
     end
   end
