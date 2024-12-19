a42444dd4efbc1b31972f72cc93bae2d329be19d
Paul Sojan <paul.eliassojan@gmail.com>
2024-12-19 15:05:32 +0530

    Added retry button if upload video to vimeo failed

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -10,4 +10,8 @@ module LessonsHelper
     vendor_response = vimeo_service.resolve_video_url(video_url)
     vendor_response['html'] if vendor_response.has_key?('html')
   end
+
+  def is_upload_pending(local_content)
+    local_content.status == "pending" && local_content.updated_at < 30.minutes.ago
+  end
 end
