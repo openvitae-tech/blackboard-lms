0dee9b854d0ec7af3b9154a3a078e236e63c3348
Paul Sojan <paul.eliassojan@gmail.com>
2024-12-27 17:35:57 +0530

    Fixed routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: :show
 
   def show
     authorize @user
