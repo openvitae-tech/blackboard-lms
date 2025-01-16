e393f0a9a5877f7c8c0d8478f8ca7cc78b458a2a
sadrisya <sadrisyalohid@gmail.com>
2025-01-16 15:50:37 +0530

    Assign course duration allignment issue fix (#518)

--- app/javascript/controllers/assigns_controller.js
+++ app/javascript/controllers/assigns_controller.js
@@ -1,30 +1,36 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = [
-        "durationContainer",
-        "customLabelContainer",
-        "dateSelector"
+  static targets = [
+    "durationContainer",
+    "customLabelContainer",
+    "dateSelector",
+    "datePicker"
+  ];
 
-    ];
+  toggle(event) {
+    this.dateSelectorTarget.disabled = !this.dateSelectorTarget.disabled;
 
-    toggle(event) {
-        this.dateSelectorTarget.disabled = !this.dateSelectorTarget.disabled;
-    
-        if (!this.dateSelectorTarget.disabled) {
-            this.durationContainerTarget.classList.remove("invisible", "opacity-0");
-        } else {
-            this.durationContainerTarget.classList.add("invisible", "opacity-0");
-        }
+    if (!this.dateSelectorTarget.disabled) {
+      this.durationContainerTarget.classList.remove("invisible", "opacity-0");
+    } else {
+      this.durationContainerTarget.classList.add("invisible", "opacity-0");
     }
+  }
 
-    datePicked(event) {
-        const selectedDate = event.target.value;
+  datePicked(event) {
+    const selectedDate = event.target.value;
 
-        if (selectedDate) {
-            const option = new Option(selectedDate, selectedDate);
-            this.dateSelectorTarget.appendChild(option);
-            this.dateSelectorTarget.value = selectedDate;
-        }
+    if (selectedDate) {
+      const option = new Option(selectedDate, selectedDate);
+      this.dateSelectorTarget.appendChild(option);
+      this.dateSelectorTarget.value = selectedDate;
     }
+  }
+
+  openDatePicker(event) {
+    this.datePickerTarget.showPicker();
+    this.datePickerTarget.addEventListener("blur", () => {
+    });
+  }
 }
