bc1856e4544fd56d50b40e8ec63851fef5c7e300
Paul Elias Sojan <paul.eliassojan@gmail.com>
2025-01-27 15:57:20 +0530

    Fixed custom JS files not bundling in create/edit lesson page (#609)

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -1,5 +1,17 @@
 const store = {
-  pendingCount: 0,
+  pendingUploads: 0,
+
+  addUpload() {
+    this.pendingUploads++;
+  },
+
+  removeUpload() {
+    this.pendingUploads = Math.max(0, this.pendingUploads - 1);
+  },
+
+  hasPendingUploads() {
+    return this.pendingUploads > 0;
+  }
 };
 
 export default store;
