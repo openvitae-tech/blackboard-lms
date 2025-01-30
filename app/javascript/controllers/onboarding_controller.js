1a5d60237408ecd3bbf255b59036d9c4f2fecfd6
sadrisya <sadrisyalohid@gmail.com>
2025-01-30 14:23:35 +0530

    Onboarding flow Introduction pages web is done

---
+++ app/javascript/controllers/onboarding_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["welcomeScreen", "step1Screen", "step2Screen", "step3Screen", "step4Screen"];
+
+  next(event) {
+    event.preventDefault();
+    
+    let currentScreen = event.currentTarget.dataset.current;
+    let nextScreen = event.currentTarget.dataset.next;
+
+    this[currentScreen + "Target"].classList.add("hidden");
+    this[nextScreen + "Target"].classList.remove("hidden");
+  }
+
+  back(event) {
+    event.preventDefault();
+
+    let currentScreen = event.currentTarget.dataset.current;
+    let previousScreen = event.currentTarget.dataset.previous;
+
+    this[currentScreen + "Target"].classList.add("hidden");
+    this[previousScreen + "Target"].classList.remove("hidden");
+  }
+
+  skip(event) {
+    event.preventDefault();
+    console.log("Skip intro")
+
+  }
+
+}
