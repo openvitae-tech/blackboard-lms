da0ebd25e4bac7df1cdf39912f82529486de69d5
Paul Sojan <paul.eliassojan@gmail.com>
2025-02-05 18:59:20 +0530

    Fixed no_console script fails if no JS files found in the staged files

--- .husky/helpers/no_console.sh
+++ .husky/helpers/no_console.sh
@@ -1,6 +1,10 @@
-if git diff --cached --name-only | grep -E '^app/javascript/.*\.js$' | xargs grep -nE 'console\.(log|warn|error)'; then
-  echo "❌ Commit rejected! Remove console statements from JavaScript files."
-  exit 1
+echo "Running no console pre-commit hook"
+
+if js_files=$(git diff --cached --name-only | grep -E '^app/javascript/.*\.js$'); then
+  if echo "$js_files" | xargs grep -nE 'console\.(log|warn|error)'; then
+    echo "❌ Commit rejected! Remove console statements from JavaScript files."
+    exit 1
+  fi
 fi
 
 exit 0
