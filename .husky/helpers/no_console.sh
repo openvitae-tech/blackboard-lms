f7a8daa17c3ff51b76a3dfc54a002d30813b82db
Paul Sojan <paul.eliassojan@gmail.com>
2025-02-05 19:14:20 +0530

    Updated the script

--- .husky/helpers/no_console.sh
+++ .husky/helpers/no_console.sh
@@ -1,7 +1,7 @@
 echo "Running no console pre-commit hook"
 
 if js_files=$(git diff --cached --name-only | grep -E '^app/javascript/.*\.js$'); then
-  if echo "$js_files" | xargs grep -nE 'console\.(log|warn|error)'; then
+  if echo "$js_files" | xargs grep -nE 'console\.(log|warn)'; then
     echo "❌ Commit rejected! Remove console statements from JavaScript files."
     exit 1
   fi
