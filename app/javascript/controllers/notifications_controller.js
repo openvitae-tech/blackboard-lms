22b637276784b318e1367a4e18eca8254649dedc
Paul Sojan <paul.eliassojan@gmail.com>
2025-02-05 20:02:25 +0530

    Created custom JS logger that logs detailed error only in dev mode

--- app/javascript/controllers/notifications_controller.js
+++ app/javascript/controllers/notifications_controller.js
@@ -1,32 +1,33 @@
 import { Controller } from "@hotwired/stimulus";
+import Logger from "utils/logger";
 
 export default class extends Controller {
-    static targets = ['notificationList', 'polingInterval']
-    path = "/notifications/count"
-    headers = { 'Accept' : 'text/vnd.turbo-stream.html' }
-    connect() {
-        const interval = parseInt(this.polingIntervalTarget.dataset.interval);
-        const checkNotificationInterval = setInterval(() => {
-            fetch(this.path, { headers: this.headers })
-                .then(response => response.text())
-                .then(html => Turbo.renderStreamMessage(html))
-                .catch(error => console.log(error))
-        }, interval * 1000);
+  static targets = ["notificationList", "polingInterval"];
+  path = "/notifications/count";
+  headers = { Accept: "text/vnd.turbo-stream.html" };
+  connect() {
+    const interval = parseInt(this.polingIntervalTarget.dataset.interval);
+    const checkNotificationInterval = setInterval(() => {
+      fetch(this.path, { headers: this.headers })
+        .then((response) => response.text())
+        .then((html) => Turbo.renderStreamMessage(html))
+        .catch((error) => Logger.error(error));
+    }, interval * 1000);
 
-        // clear the interval after 24 hours
-        setTimeout(() => {
-            clearInterval(checkNotificationInterval)
-        }, 86400 * 1000);
+    // clear the interval after 24 hours
+    setTimeout(() => {
+      clearInterval(checkNotificationInterval);
+    }, 86400 * 1000);
 
-        window.addEventListener("click",(e) => {
-            if (!this.notificationListTarget.contains(e.target)) {
-                this.close(e)
-            }
-        })
-    }
+    window.addEventListener("click", (e) => {
+      if (!this.notificationListTarget.contains(e.target)) {
+        this.close(e);
+      }
+    });
+  }
 
-    close(event) {
-        event.stopPropagation();
-        this.notificationListTarget.innerHTML = ""
-    }
+  close(event) {
+    event.stopPropagation();
+    this.notificationListTarget.innerHTML = "";
+  }
 }
