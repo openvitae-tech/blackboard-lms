204cce93bf5a1a9f94781f7db6747904592250e1
Deepak Kumar <deepakkumarnd@gmail.com>
2025-02-13 13:34:16 +0530

    Show progress during the onboarding process

--- app/javascript/controllers/onboarding_controller.js
+++ app/javascript/controllers/onboarding_controller.js
@@ -12,24 +12,20 @@ export default class extends Controller {
 
     nextScreen(event) {
         event.preventDefault();
-
         let currentScreen = event.currentTarget.dataset.current;
         let nextScreen = event.currentTarget.dataset.next;
-
         this[currentScreen + "Target"].classList.add("hidden");
         this[nextScreen + "Target"].classList.remove("hidden");
-        this.updateProgress(20);
+        this.updateProgress(5);
     }
 
     previousScreen(event) {
         event.preventDefault();
-
         let currentScreen = event.currentTarget.dataset.current;
         let previousScreen = event.currentTarget.dataset.previous;
-
         this[currentScreen + "Target"].classList.add("hidden");
         this[previousScreen + "Target"].classList.remove("hidden");
-        this.updateProgress(-20);
+        this.updateProgress(-5);
     }
 
     updateProgress(change) {
