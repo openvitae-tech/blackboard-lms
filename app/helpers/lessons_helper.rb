9dbea211d28ba6f09c3cac73a17a6da0359426c7
Deepak Kumar <deepakkumarnd@gmail.com>
2025-03-11 18:08:57 +0530

    Track time spent on each video based on playback events from the player.

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -12,6 +12,9 @@ module LessonsHelper
   end
 
   def upload_pending?(local_content)
+    # TODO: Scope for refactoring
+    # this checks upload to vimeo is pending or not. May be this is better renamed as upload_to_vimeo_pending?
+    # also the status can be better named as vimeo_upload_status instead of status.
     if local_content.video.blob.metadata['url'].nil? && local_content.status == "complete"
       true
     else
--- app/javascript/controllers/vimeo_controller.js
+++ app/javascript/controllers/vimeo_controller.js
@@ -1,51 +1,71 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["videoPlayer", "completeButton"];
+  static targets = ["vimeoLoader", "vimeoPlayer", "completeButton"];
 
   connect() {
-    this.player = new Vimeo.Player(this.videoPlayerTarget);
+    // return if the vimeo player is not present, for example in local
+    if (!this.hasVimeoPlayerTarget) return;
+
+    if (this.hasVimeoLoaderTarget) {
+      // show loader
+      this.showLoader();
+      // hide the loader once the video is loaded
+      this.vimeoPlayerTarget.addEventListener(
+          "load", () => this.hideLoader()
+      );
+    }
+
+    this.player = new Vimeo.Player(this.vimeoPlayerTarget);
 
     this.player.getDuration().then((duration) => {
-      this.completionThreshold = duration * (0.90 + Math.random() * 0.05);
+      // generate a random duration between 87% to 97% as completion threshold
+      this.completionThreshold = Math.floor((Math.random() * 11 + 87));
     });
 
-    ["play", "pause", "ended"].forEach(event => {
-      this.player.on(event, () => this.handleEvent(event));
-    });
-
-    this.player.on("timeupdate", (data) => this.checkCompletion(data.seconds));
+    this.player.on("loaded", (event) => this.handleLoadVideo(event));
+    this.player.on("play", (event) => this.handlePlayVideo(event));
+    this.player.on("pause", (event) => this.handlePauseVideo(event));
+    this.player.on("ended", (event) => this.handleEndVideo(event));
+    this.player.on("timeupdate", (event) => this.checkCompletion(event));
   }
 
-  async handleEvent(eventType) {
-    let duration = await this.player.getDuration();
-    let currentTime = eventType === "ended" ? duration : await this.player.getCurrentTime();
-    let remainingTime = duration - currentTime;
-
-    let formattedCurrentTime = this.formatTime(currentTime);
-    let formattedRemainingTime = this.formatTime(remainingTime);
-
-    alert(`Video ${eventType} at ${formattedCurrentTime}\nRemaining: ${formattedRemainingTime}`);
-
-    if (eventType === "ended") {
-      await this.handleVideoEnd();
-    }
+  async handleLoadVideo(event){
+    self.timeSpent = 0;
+    self.startTime = null;
+    this.updateTimeSpent();
   }
 
-  checkCompletion(currentTime) {
-    if (currentTime >= this.completionThreshold) {
-      this.enableCompleteButton();
-    }
+  async handlePlayVideo(event) {
+    self.startTime = new Date();
   }
 
-  handleVideoEnd() {
-    this.player.pause();
+  async handlePauseVideo(event) {
+    this.updateTimeSpent();
+  }
 
+  async handleEndVideo(event) {
+    this.updateTimeSpent();
+    this.enableCompleteButton();
+  }
+
+  updateTimeSpent() {
+    const duration = self.startTime ? new Date() - self.startTime : 0;
+    self.timeSpent = self.timeSpent + duration;
+    // set the time spent in the hidden form field of complete button
     if (this.hasCompleteButtonTarget) {
-      this.enableCompleteButton();
+      const form = this.completeButtonTarget.closest("form");
+      const time_spent = form.querySelector('input[name="time_spent"]');
+      time_spent.value = self.timeSpent / 1000;
     }
   }
 
+  checkCompletion(event) {
+    const completionPercentage = event.percent * 100 ;
+    if (completionPercentage >= this.completionThreshold) {
+      this.enableCompleteButton();
+    }
+  }
   enableCompleteButton() {
     if (this.hasCompleteButtonTarget) {
       this.completeButtonTarget.classList.remove("disabled");
@@ -53,14 +73,10 @@ export default class extends Controller {
     }
   }
 
-  formatTime(seconds) {
-    let hrs = Math.floor(seconds / 3600);
-    let mins = Math.floor((seconds % 3600) / 60);
-    let secs = Math.floor(seconds % 60);
-    return [
-      hrs.toString().padStart(2, "0"),
-      mins.toString().padStart(2, "0"),
-      secs.toString().padStart(2, "0")
-    ].join(":");
+  showLoader() {
+    this.vimeoLoaderTarget.classList.remove("hidden");
+  }
+  hideLoader() {
+    this.vimeoLoaderTarget.classList.add("hidden");
   }
 }
