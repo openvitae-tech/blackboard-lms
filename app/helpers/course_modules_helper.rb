9395836e427d5be31761bdc06345ae40a96bc3a5
Paul Sojan <paul.eliassojan@gmail.com>
2025-03-13 23:35:12 +0530

    Fixed tooltip issue

--- app/helpers/course_modules_helper.rb
+++ app/helpers/course_modules_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module CourseModulesHelper
+  def module_delete_tooltip_message(course)
+    if course.published?
+      t("course.course_module.cannot_delete_published")
+    else
+      t("course.course_module.cannot_delete_enrolled")
+    end
+  end
 end
