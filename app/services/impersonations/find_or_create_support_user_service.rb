999a4646537b1e115d4f3ecfa3eb015ebf2d9cfb
Paul Sojan <paul.eliassojan@gmail.com>
2025-03-14 12:43:53 +0530

    Updated logic to generate password for support user

--- app/services/impersonations/find_or_create_support_user_service.rb
+++ app/services/impersonations/find_or_create_support_user_service.rb
@@ -5,20 +5,21 @@ class Impersonations::FindOrCreateSupportUserService
 
   def find_or_create_user(learning_partner)
     support_user = learning_partner.users.find_by(role: "support")
-    if !support_user.present?
+    return support_user if support_user.present?
+
+      generated_password = password
       support_user = User.create!(
         name: "#{learning_partner.name} Support",
         role: "support",
         email: support_email(learning_partner.name),
-        password: Rails.application.credentials.dig(:support_user, :password),
-        password_confirmation: Rails.application.credentials.dig(:support_user, :password),
+        password: generated_password,
+        password_confirmation: generated_password,
         learning_partner:,
         team_id: learning_partner.parent_team.id,
         state: 'active'
       )
       support_user.confirm
-    end
-    support_user
+      support_user
   end
 
   private
@@ -26,5 +27,9 @@ class Impersonations::FindOrCreateSupportUserService
   def support_email(name)
     formatted_name = name.downcase.gsub(/\s+/, '')
     "support@#{formatted_name}.com"
+  end
+
+  def password
+    SecureRandom.hex(8)
   end
 end
