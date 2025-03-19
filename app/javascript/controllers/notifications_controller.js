945121459ed879780fa364783dfb0362a5066d4b
sadrisya <sadrisyalohid@gmail.com>
2025-03-19 17:58:42 +0530

    Not found Text appearing error solution

--- app/javascript/controllers/notifications_controller.js
+++ app/javascript/controllers/notifications_controller.js
@@ -7,17 +7,19 @@ export default class extends Controller {
   headers = { Accept: "text/vnd.turbo-stream.html" };
   connect() {
     const interval = parseInt(this.polingIntervalTarget.dataset.interval);
-    const checkNotificationInterval = setInterval(() => {
-      fetch(this.path, { headers: this.headers })
-        .then((response) => response.text())
-        .then((html) => Turbo.renderStreamMessage(html))
-        .catch((error) => Logger.error(error));
-    }, interval * 1000);
-
-    // clear the interval after 24 hours
     setTimeout(() => {
-      clearInterval(checkNotificationInterval);
-    }, 86400 * 1000);
+      const checkNotificationInterval = setInterval(() => {
+        fetch(this.path, { headers: this.headers })
+          .then((response) => response.text())
+          .then((html) => Turbo.renderStreamMessage(html))
+          .catch((error) => Logger.error(error));
+      }, interval * 1000);
+
+      // clear the interval after 24 hours
+      setTimeout(() => {
+        clearInterval(checkNotificationInterval);
+      }, 86400 * 1000);
+    }, 180000);
 
     window.addEventListener("click", (e) => {
       if (!this.notificationListTarget.contains(e.target)) {
