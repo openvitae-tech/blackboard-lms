9f9819dc4e75669674be41ed45f1daac1f75213e
Paul Sojan <paul.eliassojan@gmail.com>
2025-04-01 23:41:27 +0530

    Added migration to populate active_users_count for existing learning partners

--- db/migrate/20250328101009_add_active_users_count_to_learning_partners.rb
+++ db/migrate/20250328101009_add_active_users_count_to_learning_partners.rb
@@ -1,5 +1,10 @@
 class AddActiveUsersCountToLearningPartners < ActiveRecord::Migration[7.2]
-  def change
+  def up
     add_column :learning_partners, :active_users_count, :integer, default: 0, null: false
+
+    LearningPartner.find_each do |learning_partner|
+      active_users = learning_partner.users.where(state: "active")
+      learning_partner.update(active_users_count: active_users.count)
+    end
   end
 end
