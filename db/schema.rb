c7d3ae73d26c064094fd32c722f83f188c79ec0a
Paul Sojan <paul.eliassojan@gmail.com>
2025-04-02 13:47:09 +0530

    Fixed migration failing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_02_053616) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_02_080139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
