0a0c14e23c221098079923fce4e9cb2289de34b7
Paul Sojan <paul.eliassojan@gmail.com>
2025-05-13 16:17:40 +0530

    Misc fixes

--- db/migrate/20250110092513_update_user_state_for_verified_users.rb
+++ db/migrate/20250110092513_update_user_state_for_verified_users.rb
@@ -1,6 +1,6 @@
 class UpdateUserStateForVerifiedUsers < ActiveRecord::Migration[7.2]
   def up
-    # User.where(state: 'unverified').where.not(confirmed_at: nil).update_all(state: 'active')
+    User.where(state: 'unverified').where.not(confirmed_at: nil).update_all(state: 'active')
   end
 
   def down
