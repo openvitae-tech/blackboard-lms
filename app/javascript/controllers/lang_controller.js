fc703e8d8c54b0b0426cd00bb2745ce5d16f19ab
sadrisya <sadrisyalohid@gmail.com>
2025-07-01 16:24:33 +0530

    Resolved an issue where the form submission failed with the error (#889)

--- app/javascript/controllers/lang_controller.js
+++ app/javascript/controllers/lang_controller.js
@@ -3,9 +3,20 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["radio", "otherLanguageField", "otherLanguageInput"];
 
+  connect() {
+    this.toggleOtherLanguageField();
+  }
+  
   languageSelected(event) {
-    const selectedValue = event.target.value;
-
+    this.toggleOtherLanguageField(event.target.value);
+  }
+  
+  toggleOtherLanguageField(selectedValue = null) {
+    if (!selectedValue) {
+      const selected = this.radioTargets.find(radio => radio.checked);
+      selectedValue = selected?.value;
+    }
+  
     if (selectedValue === "Other") {
       this.otherLanguageFieldTarget.classList.remove("hidden");
       this.otherLanguageInputTarget.required = true;
@@ -14,4 +25,5 @@ export default class extends Controller {
       this.otherLanguageFieldTarget.classList.add("hidden");
     }
   }
+  
 }
