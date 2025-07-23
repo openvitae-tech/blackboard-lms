014a8840162f40cef703b2f12713013f4cac387a
sadrisya <sadrisyalohid@gmail.com>
2025-07-23 11:40:31 +0530

    Simplify link_text_component to use a single consistent style

--- app/helpers/typography_helper.rb
+++ app/helpers/typography_helper.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 module TypographyHelper
-  def link_text_component(text, link, html_options = {}, type: :default)
+  def link_text_component(text, link, html_options = {})
     render partial: 'ui/typography/linked_text_component', locals: {
       text:,
       link:,
-      html_options:,
-      type:
+      html_options:
     }
   end
 end
