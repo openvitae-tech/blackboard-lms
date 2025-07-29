f9fb2fcbf6097c6106afe6539d0d09e0d94afb9b
Deepak Kumar <deepakkumarnd@gmail.com>
2025-07-29 10:44:00 +0530

    Course list carousal (#957)

--- app/controllers/ui/components_controller.rb
+++ app/controllers/ui/components_controller.rb
@@ -3,4 +3,14 @@
 class Ui::ComponentsController < Ui::BaseController
   def index
   end
+
+  def app
+    @house_keeping_ctx = SearchContext.new(context: SearchContext::COURSE_LISTING, tags: ['Housekeeping'])
+    service = Courses::FilterService.new(current_user, @house_keeping_ctx)
+    @house_keeping_courses = service.filter.records
+
+    @food_production_ctx = SearchContext.new(context: SearchContext::COURSE_LISTING, tags: ['Food Production'])
+    service = Courses::FilterService.new(current_user, @food_production_ctx)
+    @food_production_courses = service.filter.records
+  end
 end
