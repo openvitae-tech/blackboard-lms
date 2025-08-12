26d8f65908dd735c8a30a5c6d1ccb924f72e04fb
Paul Sojan <paul.eliassojan@gmail.com>
2025-08-12 14:26:09 +0530

    Misc fixes

--- config/initializers/encryption.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-ENCRYPTION_KEY = Rails.application.key_generator.generate_key('default encryption',
-                                                              ActiveSupport::MessageEncryptor.key_len)
-
-DEFAULT_ENCRYPTOR = ActiveSupport::MessageEncryptor.new(ENCRYPTION_KEY)
