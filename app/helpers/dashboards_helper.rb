aef7cc027667d79b37f122a1c65e6186166cc9f9
sadrisya <sadrisyalohid@gmail.com>
2025-08-14 16:34:18 +0530

    move time_spent_series scaling logic to DashboardsHelper for view transformation

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -11,4 +11,12 @@ module DashboardsHelper
   def selected_label(duration)
     Dashboard::VALID_DURATIONS[duration.to_sym] || Dashboard::VALID_DURATIONS[:last_7_days].to_s
   end
+
+  def scale_time_spent_series(series)
+    return {} if series.blank?
+
+    max_value = series.values.max || 0
+    scale_factor = max_value > 120 ? 120.0 / max_value : 1
+    series.transform_values { |v| (v * scale_factor).round }
+  end
 end
