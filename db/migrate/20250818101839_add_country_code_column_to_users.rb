1be14d7fb6f7d44ef16b0fd63fd7e95d5349d7c8
Deepak Kumar <deepakkumarnd@gmail.com>
2025-08-22 22:32:30 +0530

    Move data migration to a separate file

--- db/migrate/20250818101839_add_country_code_column_to_users.rb
+++ db/migrate/20250818101839_add_country_code_column_to_users.rb
@@ -1,8 +1,5 @@
 class AddCountryCodeColumnToUsers < ActiveRecord::Migration[8.0]
   def change
     add_column :users, :country_code, :string
-
-    User.reset_column_information
-    User.where.not(phone: nil).update_all(country_code: "+91")
   end
 end
