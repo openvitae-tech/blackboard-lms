4487e6477a1c5178946322abc822a699a5e06677
Deepak Kumar <deepakkumarnd@gmail.com>
2025-08-22 22:37:15 +0530

    Move data migration to a separate file (#985)

--- db/migrate/20250819184851_add_supported_countries_column_to_learning_partner.rb
+++ db/migrate/20250819184851_add_supported_countries_column_to_learning_partner.rb
@@ -1,8 +1,5 @@
 class AddSupportedCountriesColumnToLearningPartner < ActiveRecord::Migration[8.0]
   def change
     add_column :learning_partners, :supported_countries, :text, array: true, default: [], null: false
-
-    LearningPartner.reset_column_information
-    LearningPartner.update_all(supported_countries: ["india"])
   end
 end
