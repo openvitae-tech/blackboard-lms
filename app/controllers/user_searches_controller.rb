3fef123ec4c74e41c00dadb55f4f7be19ccda9e4
Deepak Kumar <deepakkumarnd@gmail.com>
2025-08-27 18:06:57 +0530

    Include term used in previous search in pagination

--- app/controllers/user_searches_controller.rb
+++ app/controllers/user_searches_controller.rb
@@ -12,9 +12,10 @@ class UserSearchesController < ApplicationController
   private
 
   def load_users
-    @all_members = search_params[:all_members]
     @team = Team.find(search_params[:team_id])
-    @users = Users::FilterService.new(@team, term: search_params[:term], all_members: @all_members)
+    @all_members = search_params[:all_members]
+    @term = search_params[:term]
+    @users = Users::FilterService.new(@team, term: @term, all_members: @all_members)
                                  .filter.includes([:team]).page(params[:page]).per(5)
   end
 
