67770d667e23f28e53c7f0251b7ec035c0328463
Deepak Kumar <deepakkumarnd@gmail.com>
2025-08-27 17:51:30 +0530

    Use pagination using turbo for user listing

--- app/controllers/user_searches_controller.rb
+++ app/controllers/user_searches_controller.rb
@@ -1,15 +1,24 @@
 # frozen_string_literal: true
 
 class UserSearchesController < ApplicationController
+  def index
+    load_users
+  end
+
   def create
-    @team = Team.find(search_params[:team_id])
-    @users = Users::FilterService.new(@team, term: search_params[:term])
-                                 .filter.includes([:team]).page(params[:page]).per(5)
+    load_users
   end
 
   private
 
+  def load_users
+    @all_members = search_params[:all_members]
+    @team = Team.find(search_params[:team_id])
+    @users = Users::FilterService.new(@team, term: search_params[:term], all_members: @all_members)
+                                 .filter.includes([:team]).page(params[:page]).per(5)
+  end
+
   def search_params
-    params.except(:authenticity_token).permit(:term, :team_id)
+    params.except(:authenticity_token).permit(:term, :team_id, :all_members)
   end
 end
