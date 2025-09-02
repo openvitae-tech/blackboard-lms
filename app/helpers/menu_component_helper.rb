8eccb5a85b7c1aba0cb95ac8c0a4915490881f00
sadrisya <sadrisyalohid@gmail.com>
2025-09-02 20:10:47 +0530

    moved inline menu item definitions from views into helper methods

---
+++ app/helpers/menu_component_helper.rb
@@ -0,0 +1,25 @@
+module MenuComponentHelper
+  MenuItem = Struct.new(
+    :label,
+    :url,
+    :type,
+    :options,
+    :extra_classes,
+    :visible,
+    keyword_init: true
+  ) do
+    def visible?
+      visible != false 
+    end
+  end
+
+  def menu_component(menu_items, icon_class: '', label_hover: '', html_options: {})
+    render(
+      'ui/inputs/menu_component',
+      menu_items: menu_items.select(&:visible?),
+      icon_class:,
+      label_hover:,
+      html_options:
+    )
+  end
+end
