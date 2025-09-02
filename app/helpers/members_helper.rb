7b162072a965f2ab28d8a81f405fe5347a279f0e
sadrisya <sadrisyalohid@gmail.com>
2025-09-02 20:40:18 +0530

    Refactor helpers to reduce method length and resolve Rubocop offenses

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -14,43 +14,42 @@ module MembersHelper
 
   def deactivate_item(user)
     MenuComponentHelper::MenuItem.new(
-      label: "Deactivate",
+      label: 'Deactivate',
       url: deactivate_member_path(user),
       type: :link,
-      options: { data: { turbo_frame: "modal" } },
+      options: { data: { turbo_frame: 'modal' } },
       visible: policy(user).deactivate?
     )
   end
 
   def activate_item(user)
     MenuComponentHelper::MenuItem.new(
-      label: "Activate",
+      label: 'Activate',
       url: activate_member_path(user),
       type: :link,
-      options: { data: { turbo_frame: "modal" } },
+      options: { data: { turbo_frame: 'modal' } },
       visible: policy(user).activate?
     )
   end
 
   def delete_item(user)
     MenuComponentHelper::MenuItem.new(
-      label: "Delete",
+      label: 'Delete',
       url: member_path,
       type: :link,
-      extra_classes: "text-danger",
-      options: { data: { turbo_method: :delete, turbo_confirm: "Are you sure want to delete?" } },
+      extra_classes: 'text-danger',
+      options: { data: { turbo_method: :delete, turbo_confirm: 'Are you sure want to delete?' } },
       visible: policy(user).destroy?
     )
   end
 
   def change_team_item(user)
     MenuComponentHelper::MenuItem.new(
-      label: "Change team",
+      label: 'Change team',
       url: change_team_member_path,
       type: :link,
-      options: { data: { turbo_frame: "modal" } },
+      options: { data: { turbo_frame: 'modal' } },
       visible: policy(user).change_team?
     )
   end
 end
-
