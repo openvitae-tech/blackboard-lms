fcbf61c8f4b88d030e4edb91f929ec848b295fba
sadrisya <sadrisyalohid@gmail.com>
2025-09-03 16:00:30 +0530

    removed custom visible? from MenuItem and made menu_items a keyword argument in menu_component

--- app/helpers/menu_component_helper.rb
+++ app/helpers/menu_component_helper.rb
@@ -9,16 +9,12 @@ module MenuComponentHelper
     :extra_classes,
     :visible,
     keyword_init: true
-  ) do
-    def visible?
-      visible != false
-    end
-  end
+  )
 
-  def menu_component(menu_items, icon_class: '', label_hover: '', html_options: {})
+  def menu_component(menu_items:, icon_class: '', label_hover: '', html_options: {})
     render(
       'ui/inputs/menu_component',
-      menu_items: menu_items.select(&:visible?),
+      menu_items: menu_items.select { |item| item.visible != false },
       icon_class:,
       label_hover:,
       html_options:
