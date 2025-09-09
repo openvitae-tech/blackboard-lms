ebc95d46bb2bb398fb8f3f8b785570634685c90f
Deepak Kumar <deepakkumarnd@gmail.com>
2025-09-09 19:33:04 +0530

    Fix service specs

--- spec/services/auth/otp_service_spec.rb
+++ spec/services/auth/otp_service_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Auth::OtpService, type: :service do
   let(:phone) { MobileNumber.new(value: '9999999999', country_code: AVAILABLE_COUNTRIES[:india][:code]) }
-  let(:cache_key) { "otp:#{phone}" }
+  let(:cache_key) { "otp:#{phone.value}" }
   let(:service) { described_class.new(phone, name: 'Deepak') }
 
   before do
