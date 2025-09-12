04bf3d142fb53e05116a7b91c2785f1f1e26fd94
sadrisya <sadrisyalohid@gmail.com>
2025-09-12 17:49:01 +0530

    unified the classes and icon for the menu component

--- app/helpers/view_component/menu_component_helper.rb
+++ app/helpers/view_component/menu_component_helper.rb
@@ -11,12 +11,11 @@ module ViewComponent
       keyword_init: true
     )
 
-    def menu_component(menu_items:, icon_class: '', label_hover: '', html_options: {})
+    def menu_component(menu_items:, icon_class: '', html_options: {})
       render(
         'view_components/menu_component/menu_component',
         menu_items: menu_items,
         icon_class:,
-        label_hover:,
         html_options:
       )
     end
