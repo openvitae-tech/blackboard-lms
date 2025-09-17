ddaf329d68777d5610c897908c533b13566f02fd
Deepak Kumar <deepakkumarnd@gmail.com>
2025-09-17 17:04:48 +0530

    Add default url option for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -96,5 +96,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.dig(:app, :base_url) }
   config.action_mailer.asset_host = Rails.application.credentials.dig(:app, :base_url)
 end
