9bc945c615b5335e10cd3c25d69ad2542f62c367
sadrisya <sadrisyalohid@gmail.com>
2025-09-19 17:11:48 +0530

    Default menu item options to empty hash

--- app/helpers/view_component/menu_component_helper.rb
+++ app/helpers/view_component/menu_component_helper.rb
@@ -12,9 +12,14 @@ module ViewComponent
     )
 
     def menu_component(menu_items:, icon_class: '', html_options: {})
+      menu_items_with_options = menu_items.map do |item|
+        item.options ||= {}
+        item
+      end
+
       render(
         'view_components/menu_component/menu_component',
-        menu_items: menu_items,
+        menu_items: menu_items_with_options,
         icon_class:,
         html_options:
       )
