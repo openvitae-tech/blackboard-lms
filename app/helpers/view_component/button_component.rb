b37286dce89452644d306fb0c58543a3842a164c
Deepak Kumar <deepakkumarnd@gmail.com>
2025-09-25 22:01:47 +0530

    Add new button_component

--- app/helpers/view_component/button_component.rb
+++ app/helpers/view_component/button_component.rb
@@ -2,6 +2,11 @@
 
 module ViewComponent
   module ButtonComponent
+    def label_with_icon(icon, label_tag, position)
+      ordered = position == 'left' ? [icon, label_tag] : [label_tag, icon]
+      safe_join(ordered.compact)
+    end
+
     def button(
       label: nil,
       type: 'primary',
@@ -24,6 +29,108 @@ module ViewComponent
           tooltip_text:,
           tooltip_position:,
           disabled:,
+          html_options:
+        }
+      )
+    end
+
+    BUTTON_TYPES = %w[solid outline].freeze
+    BUTTON_SIZES = %w[sm md lg].freeze
+    COLOR_SCHEMES = %w[primary secondary danger].freeze
+    ICON_POSITIONS = %w[left right].freeze
+    TOOLTIP_POSITIONS = %w[top left bottom right].freeze
+    DISABLED_CLASS = %w[opacity-50 cursor-not-allowed].freeze
+
+    def button_color_scheme_mapping(type, scheme, disabled)
+      {
+        solid: {
+          primary: {
+            bg: disabled ? 'bg-primary-light-50' : 'bg-primary',
+            bg_hover: 'bg-primary-light',
+            border: disabled ? 'border-disabled-color' : 'border-primary',
+            border_hover: 'border-primary-light',
+            text: disabled ? 'text-disabled-color' : 'text-white-light',
+            text_hover: 'text-white'
+          },
+          secondary: {
+            bg: disabled ? 'bg-secondary-light' : 'bg-secondary',
+            bg_hover: 'bg-secondary-dark',
+            border: disabled ? 'border-disabled-color' : 'border-secondary',
+            border_hover: 'border-secondary-dark',
+            text: disabled ? 'text-disabled-color' : 'text-white',
+            text_hover: 'text-white'
+          },
+          danger: {
+            bg: disabled ? 'bg-danger-light' : 'bg-danger-dark',
+            bg_hover: 'bg-danger',
+            border: disabled ? 'border-disabled-color' : 'border-danger-dark',
+            border_hover: 'border-danger',
+            text: disabled ? 'text-disabled-color' : 'text-white-light',
+            text_hover: 'text-white'
+          }
+        },
+        outline: {
+          primary: {
+            bg: 'bg-white',
+            bg_hover: 'bg-white',
+            border: disabled ? 'border-disabled-color' : 'border-primary',
+            border_hover: 'border-primary-light',
+            text: disabled ? 'text-disabled-color' : 'text-primary',
+            text_hover: 'text-primary-light'
+          },
+          secondary: {
+            bg: 'bg-white',
+            bg_hover: 'bg-white',
+            border: disabled ? 'border-disabled-color' : 'border-secondary',
+            border_hover: 'border-secondary-dark',
+            text: disabled ? 'text-disabled-color' : 'text-secondary',
+            text_hover: 'text-secondary-dark'
+          },
+          danger: {
+            bg: 'bg-white',
+            bg_hover: 'bg-white',
+            border: disabled ? 'border-disabled-color' : 'border-danger-dark',
+            border_hover: 'border-danger',
+            text: disabled ? 'text-disabled-color' : 'text-danger-dark',
+            text_hover: 'text-danger'
+          }
+        }
+      }[type.to_sym][scheme.to_sym]
+    end
+
+    def button_component(
+      label: 'Button',
+      type: 'solid',
+      size: 'md',
+      colorscheme: 'primary',
+      icon_name: '',
+      icon_position: 'left',
+      tooltip_text: '',
+      tooltip_position: 'bottom',
+      disabled: false,
+      html_options: {}
+    )
+      # validation
+      raise "Incorrect button type: #{type}" unless BUTTON_TYPES.include? type
+      raise "Incorrect button size: #{size}" unless BUTTON_SIZES.include? size
+      raise "Incorrect color scheme: #{colorscheme}" unless COLOR_SCHEMES.include? colorscheme
+      raise "Incorrect icon_position: #{icon_position}" unless ICON_POSITIONS.include? icon_position
+      raise "Incorrect tooltip_position: #{tooltip_position}" unless TOOLTIP_POSITIONS.include? icon_position
+
+      color_mapping = button_color_scheme_mapping(type, colorscheme, disabled)
+
+      render(
+        partial: 'view_components/button_component/button',
+        locals: {
+          label:,
+          type:,
+          size:,
+          icon_name:,
+          icon_position:,
+          tooltip_text:,
+          tooltip_position:,
+          disabled:,
+          color_mapping:,
           html_options:
         }
       )
