c0fca1cc787685457c7bb36657e23e146d51d403
sadrisya <sadrisyalohid@gmail.com>
2025-09-30 21:22:44 +0530

    Textarea component is created

---
+++ app/helpers/view_component/textarea_component.rb
@@ -0,0 +1,35 @@
+module ViewComponent
+  module TextareaComponent
+    TEXTAREA_SIZES = %w[md lg].freeze
+
+    def textarea_component(
+      form: nil,
+      field_name:,
+      label:,
+      placeholder:,
+      value: nil,
+      rows: 5,
+      size: 'md',
+      html_options: {},
+      support_text: nil
+    )
+      raise "Incorrect textarea size: #{size}" unless TEXTAREA_SIZES.include?(size)
+      
+      error_message = form&.object&.errors&.[](field_name)&.first
+
+
+      render partial: 'view_components/textarea_component/textarea', locals: {
+        form:,
+        field_name:,
+        label:,
+        value:,
+        placeholder:,
+        rows:,
+        size:,
+        html_options:,
+        support_text: error_message.presence || support_text,
+        error: error_message.present?    
+      }
+    end
+  end
+end
