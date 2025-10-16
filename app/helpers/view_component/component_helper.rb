483e539145303d030311bca1dea66c66f8bed0be
Deepak Kumar <deepakkumarnd@gmail.com>
2025-10-16 17:12:06 +0530

    Add input_radio_component for radio button input (#1125)

--- app/helpers/view_component/component_helper.rb
+++ app/helpers/view_component/component_helper.rb
@@ -8,9 +8,15 @@ module ViewComponent
     end
 
     def resolve_error(form, name, explicit_error)
+      return explicit_error if explicit_error.present?
+
       errors = form&.object&.errors
-      form_error = errors&.[](name)&.first
-      form_error.presence || explicit_error
+      errors&.[](name)&.first
+    end
+
+    def arrange_tags(left_tag, right_tag, position)
+      ordered = position == 'left' ? [left_tag, right_tag] : [right_tag, left_tag]
+      safe_join(ordered.compact)
     end
   end
 end
