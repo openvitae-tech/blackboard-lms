02a96de2e7dc5e1400550265665d225d4efb748d
dr-r3d <abhishek.1984.anand@gmail.com>
2025-10-27 22:15:21 +0530

    adds specs for audio extraction job and ffmpeg service

---
+++ spec/jobs/extract_and_save_audio_job_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ExtractAndSaveAudioJob do
+  let(:job) { described_class.new }
+  let(:local_content) { instance_double(LocalContent, id: 1, video: video_attachment, audio: audio_attachment) }
+  let(:video_attachment) { double(ActiveStorage::Blob, blank?: false, attached?: true, blob: blob) }
+  let(:audio_attachment) { double(ActiveStorage::Blob) }
+  let(:blob) { double(ActiveStorage::Blob, open: nil) }
+  let(:file) { double(File, path: '/tmp/video.mp4') }
+  let(:audio_io) { StringIO.new('audio data') }
+
+  before do
+    allow(LocalContent).to receive(:find_by).with(id: local_content.id).and_return(local_content)
+    allow(local_content).to receive(:save!).and_return(true)
+    allow(blob).to receive(:open).and_yield(file)
+    allow(FfmpegService).to receive_message_chain(:instance, :extract_audio).and_return(audio_io)
+    allow(audio_attachment).to receive(:attach)
+    allow(SecureRandom).to receive(:uuid).and_return('uuid')
+  end
+
+  describe '#perform' do
+    context 'when local_content_id is nil' do
+      it 'returns without doing anything' do
+        allow(LocalContent).to receive(:find_by).and_return(nil)
+        job.perform(nil)
+      end
+    end
+
+    context 'when local_content_id is invalid' do
+      it 'returns without doing anything' do
+        allow(LocalContent).to receive(:find_by).and_return(nil)
+        job.perform(nil)
+      end
+    end
+
+    context 'when local_content video is blank' do
+      it 'returns without doing anything' do
+        allow(local_content).to receive(:video).and_return(double(blank?: true))
+        expect(local_content.audio).not_to receive(:attach)
+        job.perform(local_content.id)
+      end
+    end
+
+    context 'when local_content video is not attached' do
+      it 'returns without doing anything' do
+        allow(local_content.video).to receive(:attached?).and_return(false)
+        expect(local_content.audio).not_to receive(:attach)
+        job.perform(local_content.id)
+      end
+    end
+
+    context 'when audio extraction fails' do
+      it 'logs error and does not attach audio' do
+        allow(FfmpegService).to receive_message_chain(:instance, :extract_audio).and_return(nil)
+        expect(job).to receive(:log_error_to_sentry).with(/Audio extraction failed/)
+        expect(local_content.audio).not_to receive(:attach)
+        job.perform(local_content.id)
+      end
+    end
+
+    context 'when everything is valid' do
+      it 'attaches the extracted audio to local_content' do
+        expect(local_content.audio).to receive(:attach).with(hash_including(io: audio_io, filename: 'uuid.mp3',
+                                                                            content_type: 'audio/mpeg',
+                                                                            key: 'audio/uuid.mp3'))
+        expect(local_content).to receive(:save!)
+        job.perform(local_content.id)
+      end
+    end
+  end
+end
---
+++ spec/services/ffmpeg_service_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe FfmpegService do
+  let(:service) { described_class.instance }
+  let(:input_path) { Rails.root.join('spec/fixtures/files/sample.mp4').to_s }
+
+  before do
+    allow(File).to receive(:exist?).and_call_original
+    allow(File).to receive(:readable?).and_call_original
+  end
+
+  describe '#extract_audio' do
+    describe 'input validation' do
+      context 'when input_path is blank' do
+        it 'raises ArgumentError' do
+          expect { service.extract_audio('') }.to raise_error(ArgumentError, /Input path is required/)
+        end
+      end
+
+      context 'when input file does not exist' do
+        it 'raises ArgumentError' do
+          allow(File).to receive(:exist?).with(input_path).and_return(false)
+          expect { service.extract_audio(input_path) }.to raise_error(ArgumentError, /Input file does not exist/)
+        end
+      end
+
+      context 'when input file is not readable' do
+        it 'raises ArgumentError' do
+          allow(File).to receive(:exist?).with(input_path).and_return(true)
+          allow(File).to receive(:readable?).with(input_path).and_return(false)
+          expect { service.extract_audio(input_path) }.to raise_error(ArgumentError, /Input file is not readable/)
+        end
+      end
+    end
+
+    describe 'ffmpeg execution' do
+      context 'when ffmpeg succeeds' do
+        it 'returns a StringIO with audio data' do
+          allow(File).to receive(:exist?).with(input_path).and_return(true)
+          allow(File).to receive(:readable?).with(input_path).and_return(true)
+          allow(Open3).to receive(:popen3).and_yield(nil, StringIO.new('audio data'), StringIO.new,
+                                                     double(value: double(success?: true, exitstatus: 0)))
+          result = service.extract_audio(input_path)
+          expect(result).to be_a(StringIO)
+          expect(result.string).to eq('audio data')
+        end
+      end
+
+      context 'when ffmpeg fails' do
+        it 'raises an error' do
+          allow(File).to receive(:exist?).with(input_path).and_return(true)
+          allow(File).to receive(:readable?).with(input_path).and_return(true)
+          allow(Open3).to receive(:popen3).and_yield(nil, StringIO.new, StringIO.new('error'),
+                                                     double(value: double(success?: false, exitstatus: 1)))
+          expect { service.extract_audio(input_path) }.to raise_error(RuntimeError, /FFmpeg command failed/)
+        end
+      end
+    end
+  end
+end
