1eed097de2b8655637899254cc198d8abe22737d
Abhishek Anand <abhishek.1984.anand@gmail.com>
2025-11-03 15:56:43 +0530

    extract audio and save in local content (#1132)

---
+++ app/jobs/extract_and_save_audio_job.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class ExtractAndSaveAudioJob < BaseJob
+  include CommonsHelper
+
+  def perform(local_content_id)
+    with_tracing "local_content_id=#{local_content_id}" do
+      local_content = LocalContent.find_by(id: local_content_id)
+      return false if local_content.nil? || local_content.video.blank? || !local_content.video.attached?
+
+      local_content.video.blob.open do |file|
+        service = FfmpegService.instance
+        audio_io = service.extract_audio(file.path)
+        if audio_io.blank?
+          log_error_to_sentry("Audio extraction failed for LocalContent ID: #{local_content_id}")
+          return false
+        end
+
+        filename = "#{SecureRandom.uuid}.mp3"
+        local_content.audio.attach(
+          io: audio_io,
+          filename: filename,
+          content_type: 'audio/mpeg',
+          key: "audio/#{filename}"
+        )
+        local_content.save!
+      end
+    end
+  end
+end
--- app/services/lessons/update_service.rb
+++ app/services/lessons/update_service.rb
@@ -6,18 +6,7 @@ module Lessons
 
     def update_lesson!(lesson, lesson_params)
       lesson.update!(lesson_params)
-      upload_to_vimeo(lesson_params) if APP_CONFIG.external_video_hosting?
-    end
-
-    private
-
-    def upload_to_vimeo(lesson_params)
-      lesson_params[:local_contents_attributes].each_value do |item|
-        next if item[:blob_id].nil?
-
-        local_content = ActiveStorage::Attachment.find_by!(blob_id: item[:blob_id]).record
-        UploadVideoToVimeoJob.perform_async(item[:blob_id], local_content.id)
-      end
+      PostProcessingService.instance.process_local_contents(lesson.local_contents)
     end
   end
 end
