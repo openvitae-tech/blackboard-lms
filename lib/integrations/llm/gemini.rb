3ffaf91d6e7d0f327e4e120df5cdac3b31b44809
dr-r3d <abhishek.1984.anand@gmail.com>
2025-11-04 16:30:41 +0530

    add transcript generation to gemini

--- lib/integrations/llm/gemini.rb
+++ lib/integrations/llm/gemini.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
+# require_relative 'json_schema'
+
 module Integrations
   module Llm
     class Gemini < Api
       include CommonsHelper
+      include JsonSchema
 
       attr_accessor :model
 
       SUPPORTED_MODELS = %w[gemini-2.5-pro gemini-2.5-flash gemini-2.5-flash-lite].freeze
       DEFAULT_MODEL = 'gemini-2.5-flash'
+      TRANSCRIPTION_PROMPT = "Generate timestamped transcription for the given audio file \
+                        with output in json format. json should have start_at and end_at \
+                        timestamps in milliseconds integers for each segment."
 
       def initialize(model)
         super()
@@ -21,12 +27,19 @@ module Integrations
         ask(prompt)
       end
 
+      def generate_transcript(audio_file)
+        ask(TRANSCRIPTION_PROMPT, file_path: audio_file, response_type: :json)
+      end
+
       private
 
       def ask(prompt, file_path: nil, response_type: :text)
         service = RubyLLM.chat(model: model, provider: :gemini)
+        service = service.with_schema(TranscriptSchema) if schema_class?(response_type)
 
-        service = service.with_params(response_format: { type: 'json_object' }) if response_type == :json
+        if response_type == :json || schema_class?(response_type)
+          service = service.with_params(generationConfig: { response_mime_type: 'application/json' })
+        end
 
         response = service.ask prompt, with: file_path
 
@@ -37,6 +50,11 @@ module Integrations
         log_error_to_sentry(e.message)
 
         Result.error(e.message)
+      end
+
+      def schema_class?(response_type)
+        str_response_type = response_type.to_s.capitalize
+        Object.const_defined?(str_response_type) && Object.const_get(str_response_type) < RubyLLM::Schema
       end
     end
   end
