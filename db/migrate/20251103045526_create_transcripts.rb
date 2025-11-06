43580951c22fafd9261e226d4144d2496c1bce0f
dr-r3d <abhishek.1984.anand@gmail.com>
2025-11-06 22:01:41 +0530

    add not null constraint to transcript schema

--- db/migrate/20251103045526_create_transcripts.rb
+++ db/migrate/20251103045526_create_transcripts.rb
@@ -1,9 +1,9 @@
 class CreateTranscripts < ActiveRecord::Migration[8.0]
   def change
     create_table :transcripts do |t|
-      t.integer :start_at
-      t.integer :end_at
-      t.string :text
+      t.integer :start_at, null: false
+      t.integer :end_at, null: false
+      t.string :text, null: false
       t.references :local_content, null: false, foreign_key: true
 
       t.timestamps
