269a0d4bbb230fcb64b30e4ead784d99f6db6a0b
dr-r3d <abhishek.1984.anand@gmail.com>
2025-11-13 10:25:01 +0530

    adds quiz generation for course module via gemini

---
+++ app/services/quizzes/generation_service.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Quizzes
+  class GenerationService
+    attr_accessor :course_module, :transcripts
+
+    MAX_QUIZ_QUESTIONS = 5
+
+    def initialize(course_module)
+      @course_module = course_module
+      @transcripts = summerized_transcripts
+    end
+
+    def generate_via_ai
+      return [] if transcripts.empty?
+
+      result = Integrations::Llm::Api.llm_instance(provider: :gemini).generate_quiz(MAX_QUIZ_QUESTIONS, transcripts)
+      return result.data['quizzes'] if result.ok?
+
+      Rails.logger.error("Quiz generation failed: #{result.data}")
+      []
+    end
+
+    def max_quiz_reached?
+      course_module.quizzes.count >= MAX_QUIZ_QUESTIONS
+    end
+
+    def generate?
+      transcripts.present? && !max_quiz_reached?
+    end
+
+    def tooltip
+      if transcripts.blank?
+        'quiz.generate.transcripts_missing'
+      elsif max_quiz_reached?
+        'quiz.generate.too_many_questions'
+      else
+        'quiz.generate.button'
+      end
+    end
+
+    private
+
+    def summerized_transcripts
+      module_transcripts = course_module.lessons.includes(local_contents: :transcripts).collect do |lesson|
+        content = lesson.local_contents.first
+        {
+          title: lesson.title,
+          transcripts: (content && content.transcripts.pluck(:text).join(' ')) || ''
+        }
+      end
+      module_transcripts.select { |lt| lt[:transcripts].present? }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -114,6 +114,9 @@ Rails.application.routes.draw do
           put :moveup
           put :movedown
         end
+        collection do
+          get :generate
+        end
       end
     end
   end
