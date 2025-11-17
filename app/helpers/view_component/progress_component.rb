3136cbd8b2902e4c8d956e2e5d7474bb1fd8cd26
Deepak Kumar <deepakkumarnd@gmail.com>
2025-11-17 15:32:38 +0530

    Add progressbar to partner listing page

--- app/helpers/view_component/progress_component.rb
+++ app/helpers/view_component/progress_component.rb
@@ -2,11 +2,11 @@
 
 module ViewComponent
   module ProgressComponent
-    def progress_circle_component(numerator:, denominator:)
+    def progressbar_component(numerator:, denominator:)
       numerator = 0 if numerator.blank?
       denominator = 0 if denominator.blank?
       fill = denominator.zero? ? 0 : (numerator / denominator.to_f * 100).to_i
-      render partial: 'view_components/progress_component/progress_circle', locals: { numerator:, denominator:, fill: }
+      render partial: 'view_components/progress_component/progressbar', locals: { numerator:, denominator:, fill: }
     end
   end
 end
