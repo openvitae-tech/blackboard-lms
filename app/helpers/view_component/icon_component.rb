3507c1936ecb4c76c0f2476589a813714fb265d9
sadrisya <sadrisyalohid@gmail.com>
2025-11-26 11:29:34 +0530

    Use passed stroke_width when available, otherwise keep SVG’s stroke-width or default to 1.5 if undefined

--- app/helpers/view_component/icon_component.rb
+++ app/helpers/view_component/icon_component.rb
@@ -2,14 +2,20 @@
 
 module ViewComponent
   module IconComponent
-    def icon(icon_name, css: nil, span_css: nil, stroke_width: '1.0')
+    def icon(icon_name, css: nil, span_css: nil, stroke_width: nil)
       file = Rails.cache.fetch("icon:#{icon_name}") do
         Rails.root.join('app', 'assets', 'icons', "#{icon_name}.svg").read
       end
 
       svg = Nokogiri::HTML::DocumentFragment.parse(file).at_css('svg')
       svg['class'] = css
-      svg['stroke-width'] = stroke_width if svg['stroke-width'].blank?
+      if stroke_width.present?
+        # Use the stroke_width passed from helper
+        svg['stroke-width'] = stroke_width
+      elsif svg['stroke-width'].blank?
+        # Use default only if SVG does not have stroke-width
+        svg['stroke-width'] = '1.5'
+      end
       content_tag(:span, svg.to_html.html_safe, class: "inline-flex justify-center items-center #{span_css}".strip) # rubocop:disable Rails/OutputSafety
     end
   end
