943a6b4ec19aba340ed4ddd8bc06b29aa379de51
sadrisya <sadrisyalohid@gmail.com>
2025-12-11 13:19:12 +0530

    remove redundant learning_partner arguments and use team.learning_partner

--- app/helpers/view_component/member_list_component.rb
+++ app/helpers/view_component/member_list_component.rb
@@ -2,13 +2,12 @@
 
 module ViewComponent
   module MemberListComponent
-    def member_list_component(learning_partner:, team:, members:, all_members: false, term: '')
-      render partial: 'view_components/member_list/member_list',
-             locals: { learning_partner:, team:, members:, all_members:, term: }
+    def member_list_component(team:, members:, all_members: false, term: '')
+      render partial: 'view_components/member_list/member_list', locals: { team:, members:, all_members:, term: }
     end
 
-    def _member_list_member_search_component(learning_partner:, team:, all_members:)
-      render partial: 'view_components/member_list/member_search', locals: { learning_partner:, team:, all_members: }
+    def _member_list_member_search_component(team:, all_members:)
+      render partial: 'view_components/member_list/member_search', locals: { team:, all_members: }
     end
 
     def member_list_members_component(team:, members:, all_members: false, term: '')
