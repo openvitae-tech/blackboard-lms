e25834d7310d655d43597c73b660511ea17f2103
sadrisya <sadrisyalohid@gmail.com>
2026-01-07 20:09:50 +0530

    rename Stimulus targets and handler for course description toggle

--- app/javascript/controllers/course_description_controller.js
+++ app/javascript/controllers/course_description_controller.js
@@ -1,26 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["content", "toggle"]
+  static targets = ["courseDescription", "showMoretoggle"]
   static values = {
     clampClass: String
   }
 
   connect() {
-    // Hide "Show more" if content does not overflow
     if (!this.isOverflowing()) {
-      this.toggleTarget.classList.add("hidden")
+      this.showMoretoggleTarget.classList.add("hidden")
     }
   }
 
-  toggle() {
-    this.contentTarget.classList.toggle(this.clampClassValue)
-
-    const isClamped = this.contentTarget.classList.contains(this.clampClassValue)
-    this.toggleTarget.textContent = isClamped ? "Show more" : "Show less"
+  showMoretoggle() {
+    this.courseDescriptionTarget.classList.toggle(this.clampClassValue)
+    const isClamped = this.courseDescriptionTarget.classList.contains(this.clampClassValue)
+    this.showMoretoggleTarget.textContent = isClamped ? "Show more" : "Show less"
   }
 
   isOverflowing() {
-    return this.contentTarget.scrollHeight > this.contentTarget.clientHeight
+    return this.courseDescriptionTarget.scrollHeight > this.courseDescriptionTarget.clientHeight
   }
 }
