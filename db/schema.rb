aa493d48590d576607e7d46a68b2b2c8e5fe7766
dr-r3d <abhishek.1984.anand@gmail.com>
2026-01-16 07:11:23 +0530

    adds unique index to issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_15_233648) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_16_013946) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -177,6 +177,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_15_233648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_issues_on_question_id"
+    t.index ["user_id", "question_id"], name: "index_issues_on_user_id_and_question_id", unique: true
     t.index ["user_id"], name: "index_issues_on_user_id"
   end
 
