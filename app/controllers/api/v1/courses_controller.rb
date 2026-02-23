87871be1c46fe23f6a1f2fd737310951d7725cca
Deepak <deepakkumarnd@gmail.com>
2026-02-23 20:08:35 +0530

    Add eager loading for the course while sending the full course json object.

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -4,7 +4,8 @@ module Api
   module V1
     class CoursesController < ApiController
       def show
-        course = Course.includes(:tags).find(params[:id])
+        course = Course.includes(:tags, course_modules: { lessons: :local_contents })
+          .find(params[:id])
         render json: course.to_json_data, status: :ok
       rescue ActiveRecord::RecordNotFound
         render json: { error: 'Course not found' }, status: :not_found
