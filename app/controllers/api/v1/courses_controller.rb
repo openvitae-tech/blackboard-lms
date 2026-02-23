2ff226492d9a4b6a5599e4119e8b52760f2e8eb9
Deepak <deepakkumarnd@gmail.com>
2026-02-23 19:48:24 +0530

    Add eager loading of tags

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class CoursesController < ApiController
       def show
-        course = Course.find(params[:id])
+        course = Course.includes(:tags).find(params[:id])
         render json: course.to_json_data, status: :ok
       rescue ActiveRecord::RecordNotFound
         render json: { error: 'Course not found' }, status: :not_found
