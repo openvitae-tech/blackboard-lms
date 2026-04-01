6c336ddfc991668c0724633905e4d852a90677dd
sadrisya <sadrisyalohid@gmail.com>
2026-04-01 22:04:48 +0530

    Add clear search button and fix search filter preservation after program delete

--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -14,7 +14,7 @@ module ProgramsHelper
     if show_delete
       items << ViewComponent::MenuComponentHelper::MenuItem.new(
         label: t('button.delete'),
-        url: confirm_destroy_program_path(program),
+        url: confirm_destroy_program_path(program, term: params[:term]),
         type: :link,
         extra_classes: 'text-danger',
         options: { data: { turbo_frame: 'modal' } }
