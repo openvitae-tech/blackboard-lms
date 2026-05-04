9617139f612a377ca7a69d4b836e6a6b30fdae81
sadrisya <sadrisyalohid@gmail.com>
2026-05-04 15:41:00 +0530

    added option to give different colour to the progress bar

--- engines/neo_component/app/helpers/view_component/progress_component.rb
+++ engines/neo_component/app/helpers/view_component/progress_component.rb
@@ -2,11 +2,16 @@
 
 module ViewComponent
   module ProgressComponent
-    def progressbar_component(numerator:, denominator:)
-      numerator = 0 if numerator.blank?
-      denominator = 0 if denominator.blank?
-      fill = denominator.zero? ? 0 : (numerator / denominator.to_f * 100).to_i
-      render partial: 'view_components/progress_component/progressbar', locals: { numerator:, denominator:, fill: }
+    def progressbar_component(numerator: nil, denominator: nil, value: nil, color: :primary)
+      fill = if value
+               value.to_i.clamp(0, 100)
+             elsif numerator.blank? || denominator.blank? || denominator.to_f.zero?
+               0
+             else
+               (numerator / denominator.to_f * 100).to_i
+             end
+      render partial: 'view_components/progress_component/progressbar',
+             locals: { numerator:, denominator:, fill:, color:, full_width: value.present? }
     end
   end
 end
