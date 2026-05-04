5db618a9d2dd5110497fc2dd19a25c3cff6fe132
sadrisya <sadrisyalohid@gmail.com>
2026-05-04 16:07:59 +0530

    added option to have multiple progress in a single bar

--- engines/neo_component/app/helpers/view_component/progress_component.rb
+++ engines/neo_component/app/helpers/view_component/progress_component.rb
@@ -2,8 +2,10 @@
 
 module ViewComponent
   module ProgressComponent
-    def progressbar_component(numerator: nil, denominator: nil, value: nil, color: :primary)
-      fill = if value
+    def progressbar_component(numerator: nil, denominator: nil, value: nil, color: :primary, segments: nil)
+      fill = if segments
+               nil
+             elsif value
                value.to_i.clamp(0, 100)
              elsif numerator.blank? || denominator.blank? || denominator.to_f.zero?
                0
@@ -11,7 +13,8 @@ module ViewComponent
                (numerator / denominator.to_f * 100).to_i
              end
       render partial: 'view_components/progress_component/progressbar',
-             locals: { numerator:, denominator:, fill:, color:, full_width: value.present? }
+             locals: { numerator:, denominator:, fill:, color:, segments:,
+                       full_width: value.present? || segments.present? }
     end
   end
 end
