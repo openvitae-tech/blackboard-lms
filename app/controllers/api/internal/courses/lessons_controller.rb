2d87e20c602eb640d1325390eec454c8b6cf43b5
dr-r3d <abhishek.1984.anand@gmail.com>
2026-05-19 22:43:45 +0530

    Guard modules against null value in LessonsController#show
    data.fetch('modules', []) only defaults when key is absent; use
    data['modules'] || [] to also handle explicit null from NeoAI.

--- app/controllers/api/internal/courses/lessons_controller.rb
+++ app/controllers/api/internal/courses/lessons_controller.rb
@@ -6,9 +6,9 @@ module Api
       class LessonsController < BaseController
         def show
           course_data = neo_ai.find_course(params[:course_id])
-          lesson_data = course_data.fetch('modules', [])
-                                   .flat_map { |m| m.fetch('lessons', []) }
-                                   .find { |l| l['id'] == params[:id] }
+          lesson_data = (course_data['modules'] || [])
+                        .flat_map { |m| m.fetch('lessons', []) }
+                        .find { |l| l['id'] == params[:id] }
           return head :not_found if lesson_data.nil?
 
           render json: serialize_lesson(lesson_data)
