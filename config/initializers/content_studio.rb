1ae61222e734d3b95c9d22a305804ada5191ca83
dr-r3d <abhishek.1984.anand@gmail.com>
2026-05-20 12:14:37 +0530

    Initialise ContentStudio.base_url from app credentials

--- config/initializers/content_studio.rb
+++ config/initializers/content_studio.rb
@@ -3,4 +3,6 @@
 ContentStudio.authorization_callback = lambda { |user|
   user.content_studio_creator? && user.learning_partner.content_studio_enabled?
 }
-ContentStudio.public_host = Rails.application.credentials.dig(:app, :base_url) || ContentStudio.public_host
+app_base_url = Rails.application.credentials.dig(:app, :base_url)
+ContentStudio.base_url = app_base_url || ContentStudio.base_url
+ContentStudio.public_host = app_base_url || ContentStudio.public_host
