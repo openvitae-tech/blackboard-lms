9df85f495323dd79ec2bf1b220333beec6d85c61
sadrisya <sadrisyalohid@gmail.com>
2026-06-01 16:46:04 +0530

    Move modal attribute merging into helper, use content_tag in template

--- engines/neo_component/app/helpers/view_component/modal_box_component.rb
+++ engines/neo_component/app/helpers/view_component/modal_box_component.rb
@@ -16,16 +16,21 @@ module ViewComponent
       modal = ModalBoxComponent.new(title:, variant:)
       block_content = capture(&) if block_given?
 
-      container_class =
-        if variant == :success
-          'modal-success'
-        else
-          'modal-container'
-        end
+      container_class = variant == :success ? 'modal-success' : 'modal-container'
+
+      wrapper_class = ['modal-wrapper', wrapper_html_options[:class]].compact.join(' ')
+      merged_wrapper_options = { data: { controller: 'modals', modals_target: 'modalBox' } }
+                                  .deep_merge(wrapper_html_options.except(:class))
+                                  .merge(class: wrapper_class)
+
+      content_class = ['modal-content', html_options[:class]].compact.join(' ')
+      merged_content_options = html_options.except(:class).merge(class: content_class)
 
       render partial: 'view_components/modal_component/modal_box_component',
-             locals: { modal:, body: block_content, modal_footer:, container_class:, html_options:,
-                       wrapper_html_options:, close_action: }
+             locals: { modal:, body: block_content, modal_footer:, container_class:,
+                       wrapper_html_options: merged_wrapper_options,
+                       html_options: merged_content_options,
+                       close_action: }
     end
   end
 end
